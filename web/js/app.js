5d0582868f4dbc6b4723f80034c7a7e014744aa9
Nick Mudge <nick@perfectabstractions.com>
2020-08-29 20:16:12 -0400

    website

--- web/js/app.js
+++ web/js/app.js
@@ -6,9 +6,16 @@ import { ethers } from './ethers-5.0.esm.min.js'
 // A Web3Provider wraps a standard Web3 provider, which is
 // what Metamask injects as window.ethereum into each page
 const provider = new ethers.providers.Web3Provider(window.ethereum)
-const abi = ['function getAavegotchi(uint) view returns (string memory)']
+const abi = [
+  'function mintAavegotchi(bytes32)',
+  'function getAavegotchi(uint) view returns (string memory)',
+  'function mintWearables()',
+  'function transferToParent(address,address,uint,uint,uint)',
+  'function transferFromParent(address,uint,address,uint,uint)',
+  'function transferAsChild(address,uint,address,uint,uint,uint)'
+]
 const address = '0x201Df88D8d79ACA0AA6360F02eb9dD8aefdB1dfb'
-const aavegotchiNFT = new ethers.Contract(address, abi, provider)
+const ag = new ethers.Contract(address, abi, provider)
 
 // Integrating RunCSS with Webscript
 function createElement (type, props, ...children) {
@@ -20,18 +27,19 @@ function createElement (type, props, ...children) {
 
 const { div, body } = builders(createElement)
 
-// const { svg } = builders(createSVGElement)
+async function main () {
+  const svgDiv = div.class`max-w-xl mx-auto mt-5`(
+    await ag.getAavegotchi(0)
+  )
 
-const svgDiv = div.class`max-w-xl mx-auto mt-5``Switch to Kovan Network`
+  const app = div.class`container mx-auto`(
+    svgDiv
+  )
+  document.body = body(app)
+}
 
-const app = div.class`container mx-auto`(
-  svgDiv
-)
-
-document.body = body(app)
-
-aavegotchiNFT.getAavegotchi(0).then(result => {
-  svgDiv.innerHTML = result
+main().then(result => {
+  console.log('success')
 }, error => {
   console.log('yikers: ' + error)
 })
