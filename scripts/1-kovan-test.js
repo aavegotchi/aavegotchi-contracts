338411e9df4103cb25cb3545755506c0141f50d2
Nick Mudge <nick@perfectabstractions.com>
2020-09-29 11:32:38 -0400

    readme

---
+++ scripts/1-kovan-test.js
@@ -0,0 +1,21 @@
+/* global ethers */
+
+// We require the Buidler Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+// When running the script with `buidler run <script>` you'll find the Buidler
+// Runtime Environment's members available in the global scope.
+// eslint-disable-next-line no-unused-vars
+const bre = require("@nomiclabs/buidler");
+
+// const util = require('./diamond-util.js')
+
+async function main() {}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
