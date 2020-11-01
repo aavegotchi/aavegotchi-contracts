3fe6dbd3ffb428a10018b9cab35afa7b664b50df
Nick Mudge <nick@perfectabstractions.com>
2020-11-01 00:59:17 -0400

    reduced SVG output of portalAavegotchisSvg

--- contracts/Aavegotchi/facets/AavegotchiFacet.sol
+++ contracts/Aavegotchi/facets/AavegotchiFacet.sol
@@ -153,7 +153,7 @@ contract AavegotchiFacet {
             svg_[i] = string(
                 abi.encodePacked(
                     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">',
-                    getAavegotchiSvgLayers(collateralType, numericTraits),
+                    //getAavegotchiSvgLayers(collateralType, numericTraits),
                     "</svg>"
                 )
             );
