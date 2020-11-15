7ceb6400110860242f71310e3a588daaa4824416
Coderdan <coderdannn@gmail.com>
2020-11-15 17:04:05 +0800

    Added some todos to ShopFacet

--- contracts/Aavegotchi/facets/ShopFacet.sol
+++ contracts/Aavegotchi/facets/ShopFacet.sol
@@ -8,4 +8,11 @@ import "../../shared/libraries/LibERC20.sol";
 
 contract ShopFacet {
     AppStorage internal s;
+
+    //To do: Allow users to purchase items from store using GHST
+    //Purchasing items should distribute an amount of GHST to various addresses, while burning the rest
+
+    //To do: Allow users to convert vouchers for same-Id wearables
+    //Burn the voucher
+    //Mint the wearable and transfer to user
 }
