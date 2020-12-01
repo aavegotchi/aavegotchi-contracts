22dc392de0f895f806f5d33acfecbfe06793a0a3
Nick Mudge <nick@perfectabstractions.com>
2020-12-01 12:37:07 -0500

    Added approveAavegotchiDiamond function to CollateralEscrow, removed selfdestruct

--- contracts/Aavegotchi/CollateralEscrow.sol
+++ contracts/Aavegotchi/CollateralEscrow.sol
@@ -4,9 +4,20 @@ pragma experimental ABIEncoderV2;
 
 import "../shared/interfaces/IERC20.sol";
 
+struct AppStorage {
+    address owner;
+}
+
 contract CollateralEscrow {
+    AppStorage internal s;
+
     constructor(address _aTokenContract) {
-        IERC20(_aTokenContract).approve(msg.sender, uint256(-1));
-        selfdestruct(msg.sender);
+        s.owner = msg.sender;
+        approveAavegotchiDiamond(_aTokenContract);
+    }
+
+    function approveAavegotchiDiamond(address _aTokenContract) public {
+        require(msg.sender == s.owner, "CollateralEscrow: Not owner of contract");
+        IERC20(_aTokenContract).approve(s.owner, type(uint256).max);
     }
 }
