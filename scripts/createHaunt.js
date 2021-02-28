8e2c0249e378e51cbef03b20120d9957a384a8e0
Nick Mudge <nick@perfectabstractions.com>
2021-02-28 18:24:34 -0500

    Added createHaunt.js script

---
+++ scripts/createHaunt.js
@@ -0,0 +1,21 @@
+/* global ethers hre */
+
+async function main () {
+  const aavegotchiDiamondAddress = ''
+  const hauntSize = 10000
+  const price = ethers.utils.parseEther('100')
+  const daoFacet = await ethers.getContractAt('DAOFacet', aavegotchiDiamondAddress)
+  const tx = await daoFacet.createHaunt(hauntSize, price, '0x000000')
+  const receipt = await tx.wait()
+  console.log('Haunt created:', tx.hash)  
+  
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
