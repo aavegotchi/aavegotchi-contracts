47a9ecb9ea0491ea17165e83b80c5490f89079f3
Nick Mudge <nick@perfectabstractions.com>
2021-03-31 18:36:50 -0400

    Improved examples of calling functions to move assets from Ethereum

--- scripts/bridge.js
+++ scripts/bridge.js
@@ -2,37 +2,36 @@
 /* global ethers */
 
 async function main () {
-  // const abi = ['function depositFor(address user, address rootToken, bytes calldata depositData) external']
-  // const diamondAddress = 'the address'
-  // const rootChainManagerAddress = '0xA0c68C638235ee32657e8f720a23ceC1bFc77C77'
-  // const user = 'the address'
-
-  // const rootChainManager = await ethers.getContractAt(abi, rootChainManagerAddress)
-
-  // const tokenIds = [1, 2, 3, 4, 5, 6]
-  // const tokenIdsBytes = ethers.utils.defaultAbiCoder.encode(['uint256[]'], [tokenIds])
-  // const depositData = ethers.utils.defaultAbiCoder.encode(['uint256', 'bytes'], [721, tokenIdsBytes])
-  // const tx = await rootChainManager.depositFor(user, diamondAddress, depositData)
-  // const receipt = await tx.wait()
-  // if (receipt.status) {
-  //   console.log('Depoist successful')
-  // } else {
-  //   console.log('Deposit Failed')
-  // }
-
-  const abi = ['function depositFor(address user, address rootToken, bytes calldata depositData) external']
   const diamondAddress = 'the address'
   const rootChainManagerAddress = '0xA0c68C638235ee32657e8f720a23ceC1bFc77C77'
   const user = 'the address'
-
+  const abi = ['function depositFor(address user, address rootToken, bytes calldata depositData) external']
   const rootChainManager = await ethers.getContractAt(abi, rootChainManagerAddress)
+  let depositData
+  let tx
+  let receipt
 
+  /// ////////////////////////////////////////////////////////////
+  // Moving ERC721 Aavegotchi items from Ethereum to Polygon
+  const tokenIds = [1, 2, 3, 4, 5, 6]
+  const tokenIdsBytes = ethers.utils.defaultAbiCoder.encode(['uint256[]'], [tokenIds])
+  depositData = ethers.utils.defaultAbiCoder.encode(['uint256', 'bytes'], [721, tokenIdsBytes])
+  tx = await rootChainManager.depositFor(user, diamondAddress, depositData)
+  receipt = await tx.wait()
+  if (receipt.status) {
+    console.log('Depoist successful')
+  } else {
+    console.log('Deposit Failed')
+  }
+
+  /// ////////////////////////////////////////////////////////////
+  // Moving ERC1155 Aavegotchi items from Ethereum to Polygon
   const itemTypes = [1, 2, 3, 4, 5, 6]
   const itemAmounts = [100, 35, 78, 3, 99, 2000]
   const itemBytes = ethers.utils.defaultAbiCoder.encode(['uint256[]', 'uint256[]'], [itemTypes, itemAmounts])
-  const depositData = ethers.utils.defaultAbiCoder.encode(['uint256', 'bytes'], [1155, itemBytes])
-  const tx = await rootChainManager.depositFor(user, diamondAddress, depositData)
-  const receipt = await tx.wait()
+  depositData = ethers.utils.defaultAbiCoder.encode(['uint256', 'bytes'], [1155, itemBytes])
+  tx = await rootChainManager.depositFor(user, diamondAddress, depositData)
+  receipt = await tx.wait()
   if (receipt.status) {
     console.log('Depoist successful')
   } else {
