8ad6922499b31f664032ef5267a91573a5c62672
Coderdan <coderdannn@gmail.com>
2021-04-05 18:07:11 +0800

    -Added Raffle 4 wearable sets -Add wearable set updates -Add itemType updates

---
+++ scripts/updates/wearableSets/wearableSetsRaffle4.js
@@ -0,0 +1,123 @@
+//All the new sets of Raffle 4
+
+const wearableSetsRaffle4 = [
+  {
+    setId: 71,
+    name: "Snapshot Voter",
+    wearableIds: [137, 138, 139],
+    traitsBonuses: [3,0,-2,0,0],
+    allowedCollaterals: []
+  },
+  {
+    setId: 72,
+    name: "Polygonist",
+    wearableIds: [134, 135, 136],
+    traitsBonuses: [3,0,-1,0,1],
+    allowedCollaterals: []
+  },
+  {
+    setId: 73,
+    name: "Quickswap Dragon",
+    wearableIds: [130, 131, 132],
+    traitsBonuses: [3,0,1,1,0],
+    allowedCollaterals: []
+  },
+  {
+    setId: 74,
+    name: "Swappy the Dragon",
+    wearableIds: [130, 132, 133],
+    traitsBonuses: [4,0,1,1,0],
+    allowedCollaterals: []
+  },
+  {
+    setId: 75,
+    name: "Gotchi Elf",
+    wearableIds: [140,141,142],
+    traitsBonuses: [3,0,0,-1,1],
+    allowedCollaterals: []
+  },
+  {
+    setId: 76,
+    name: "Gotchi Princess",
+    wearableIds: [140, 141, 142, 143],
+    traitsBonuses: [4,1,1,0,0],
+    allowedCollaterals: []
+  },
+  {
+    setId: 77,
+    name: "Gotchi Queen",
+    wearableIds: [140, 141, 144, 143],
+    traitsBonuses: [5,0,0,-2,1],
+    allowedCollaterals: []
+  },
+  {
+    setId: 78,
+    name: "Godli Locks",
+    wearableIds: [140, 141, 145, 143],
+    traitsBonuses: [6,0,0,-2,2],
+    allowedCollaterals: []
+  },
+  {
+    setId: 79,
+    name: "Gotchi Baron",
+    wearableIds: [146, 147, 148],
+    traitsBonuses: [3, -1, 0,0,-1],
+    allowedCollaterals: []
+  },
+  {
+    setId: 80,
+    name: "Gotchi Lord",
+    wearableIds: [146, 147, 148, 150],
+    traitsBonuses: [5, -1, 0,0,-2],
+    allowedCollaterals: []
+  },
+  {
+    setId: 81,
+    name: "Gotchi King",
+    wearableIds: [146, 149, 148, 150],
+    traitsBonuses: [5,-2,0,0-1],
+    allowedCollaterals: []
+  },
+  {
+    setId: 82,
+    name: "Gotchi Emperor",
+    wearableIds: [146, 149, 148, 150, 156],
+    traitsBonuses: [6, -2,0,0-2],
+    allowedCollaterals: []
+  },
+  {
+    setId: 83,
+    name: "Lil Pumpagotchi",
+    wearableIds: [157, 158, 159, 160, 161],
+    traitsBonuses: [6,2,2,0,0],
+    allowedCollaterals: []
+  },
+  {
+    setId: 84,
+    name: "Soundcloud Rapper",
+    wearableIds: [108, 157, 158, 159, 160],
+    traitsBonuses: [5,1,2,0,0],
+    allowedCollaterals: []
+  },
+  {
+    setId: 85,
+    name: "REALM Tycoon",
+    wearableIds: [84, 85, 86, 146],
+    traitsBonuses: [4,-1,-1,0,0],
+    allowedCollaterals: []
+  },
+  {
+    setId: 86,
+    name: "Yegres the Dragon",
+    wearableIds: [14, 131, 132, 44],
+    traitsBonuses: [5,0,0,1,2],
+    allowedCollaterals: []
+  },
+  {
+    setId: 87,
+    name: "Vacation Santa",
+    wearableIds: [71, 114, 120, 117],
+    traitsBonuses: [5, -1, -1, -1, 0],
+    allowedCollaterals: []
+  }
+ ]
