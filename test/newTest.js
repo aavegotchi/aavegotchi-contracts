c341b4c887c5f74f2d3eb6009f94d211d1c07bb1
Mark Watson <64367041+mjwatson10@users.noreply.github.com>
2021-04-07 18:18:22 -0700

    test getting error voidsigner

--- test/newTest.js
+++ test/newTest.js
@@ -6,10 +6,11 @@ describe("Shopping  ", () => {
       itemsFacet,
       daoFacet,
       libAavegotchi,
-      ghstFacet,
       aavegotchiDiamondAddress,
       signer,
+      buyer,
       owner,
+      addr0,
       addr1;
 
 
@@ -20,9 +21,8 @@ describe("Shopping  ", () => {
     itemsFacet = await ethers.getContractAt("contracts/Aavegotchi/facets/ItemsFacet.sol:ItemsFacet", aavegotchiDiamondAddress);
     daoFacet = await ethers.getContractAt("contracts/Aavegotchi/facets/DAOFacet.sol:DAOFacet", aavegotchiDiamondAddress);
     libAavegotchi = await ethers.getContractAt("LibAavegotchi", aavegotchiDiamondAddress);
-    ghstFacet = await ethers.getContractAt("GHSTFacet", aavegotchiDiamondAddress);
 
-    [owner, addr1] = await ethers.getSigners();
+    [addr0, addr1] = await ethers.getSigners();
     owner = await (await ethers.getContractAt("OwnershipFacet", aavegotchiDiamondAddress)).owner();
 
     await hre.network.provider.request({
@@ -30,37 +30,53 @@ describe("Shopping  ", () => {
        params: [owner]
      }
     );
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: ["0x096c5ccb33cfc5732bcd1f3195c13dbefc4c82f4"]}
+    );
 
-    signer = ethers.provider.getSigner(owner);
+    signer = await ethers.provider.getSigner(owner);
+    buyer = await ethers.provider.getSigner("0x096c5ccb33cfc5732bcd1f3195c13dbefc4c82f4");
+
   });
 
+
   it.only("Should purchase items with GHST", async () => {
+
     // console.log("itemsFacet", itemsFacet);
-    // let balances = await items.itemBalances(addr1.address);
+    // let balances = await itemsFacet.itemBalances(addr1.address);
+
+    console.log("buyer", buyer._address);
 
     // expect(balances[1]).to.equal(10);
     // await daoFacet.connect(owner).setDao(addr1, addr1);
     await (await daoFacet.connect(signer)).updateItemTypeMaxQuantity(['129'], ['1000']);
 
-    await ghstFacet.mintTo(addr1.address);
-
-    await shopFacet.purchaseItemsWithGhst(addr1.address, ['129'], ['10']);
-    let balances = await itemsFacet.itemBalances(addr1.address);
-    console.log("balance", balances);
+    await shopFacet.connect(buyer._address).purchaseItemsWithGhst(buyer._address, ['129'], ['1'], { gasLimit: 8000000 });
+    // let balances = await itemsFacet.itemBalances(addr1.address);
+    // console.log("balance", balances);
     // expect(balances[1]).to.equal(20);
   });
 
-  it("Should NOT purchase items because items cannot be purchased with GHST", async () => {
-    await expect(shopFacet.purchaseItemsWithGhst(addr1.address, ['1'], ['10'])).to.be.reverted;
+  it("Should NOT purchase items because item NOT permitted to be purchased with GHST", async () => {
+    await expect(shopFacet.purchaseItemsWithGhst(buyer._address, ['1'], ['10'])).to.be.reverted;
   });
 
+  // it.only("Should NOT purchase items because items can NOT transferred to 0 address", async () => {
+  //   console.log("Address 0", addr0.address);
+  //
+  //   await (await daoFacet.connect(signer)).updateItemTypeMaxQuantity(['129'], ['1000']);
+  //
+  //   await shopFacet.purchaseItemsWithGhst(addr0.address, ['129'], ['10']);
+  // });
+
   it("Should NOT purchase items because total item type quantity exceeds max quantity", async () => {
-    await expect(shopFacet.purchaseItemsWithGhst(addr1.address, ['90'], ['10'])).to.be.reverted;
+    await expect(shopFacet.purchaseItemsWithGhst(buyer._address, ['90'], ['10'])).to.be.reverted;
   });
 
   it("Should NOT purchase items because itemIds.length is NOT equal to quantities.length", async () => {
-    await expect(shopFacet.purchaseItemsWithGhst(addr1.address, ['90'], ['10', '1'])).to.be.reverted;
-    await expect(shopFacet.purchaseItemsWithGhst(addr1.address, ['90', '129'], ['10'])).to.be.reverted;
+    await expect(shopFacet.purchaseItemsWithGhst(buyer._address, ['90'], ['10', '1'])).to.be.reverted;
+    await expect(shopFacet.purchaseItemsWithGhst(buyer._address, ['90', '129'], ['10'])).to.be.reverted;
   });
 
   it("Should NOT purchase items with GHST because address does NOT have enough GHST to purchase item", async () => {
