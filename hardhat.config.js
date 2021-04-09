1696b1911f193490a6c4b386b079bff4fc4d7806
Mark Watson <64367041+mjwatson10@users.noreply.github.com>
2021-04-09 09:50:11 -0700

    purchase transfer event
    logs amount of times unique addresses have purchased items 159 or 160

--- hardhat.config.js
+++ hardhat.config.js
@@ -36,15 +36,15 @@ module.exports = {
     // localhost: {
     //   timeout: 160000
     // },
-    matic: {
-      url: process.env.MATIC_URL,
-      // url: 'https://rpc-mainnet.maticvigil.com/',
-      accounts: [process.env.SECRET],
-      // blockGasLimit: 20000000,
-      blockGasLimit: 20000000,
-      gasPrice: 1000000000,
-      timeout: 90000
-    }
+    // matic: {
+    //   url: process.env.MATIC_URL,
+    //   // url: 'https://rpc-mainnet.maticvigil.com/',
+    //   accounts: [process.env.SECRET],
+    //   // blockGasLimit: 20000000,
+    //   blockGasLimit: 20000000,
+    //   gasPrice: 1000000000,
+    //   timeout: 90000
+    // }
     // mumbai: {
     //   url: 'https://rpc-mumbai.matic.today',
     //   accounts: [process.env.SECRET],
