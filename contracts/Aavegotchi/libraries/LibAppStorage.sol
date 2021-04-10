28a085237b244676a674e4ed32081b34262b698b
mudgen <nick@perfectabstractions.com>
2021-04-10 06:18:14 -0400

    Implementing pet operator functionality

--- contracts/Aavegotchi/libraries/LibAppStorage.sol
+++ contracts/Aavegotchi/libraries/LibAppStorage.sol
@@ -228,6 +228,8 @@ struct AppStorage {
     mapping(address => mapping(uint256 => mapping(address => uint256))) erc721TokenToListingId;
     // body wearableId => sleevesId
     mapping(uint256 => uint256) sleeves;
+    mapping(address => mapping(uint256 => address)) petOperators;
+    mapping(address => uint256[]) petOperatorTokenIds;
 }
 
 library LibAppStorage {
