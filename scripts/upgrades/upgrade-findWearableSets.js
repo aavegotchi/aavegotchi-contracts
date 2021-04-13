0841b766a5bbeb87e2c73af8d54593880efce358
mudgen <nick@perfectabstractions.com>
2021-04-13 06:57:18 -0400

    Prepared upgrade

--- scripts/upgrades/upgrade-findWearableSets.js
+++ scripts/upgrades/upgrade-findWearableSets.js
@@ -38,50 +38,50 @@ async function main () {
     throw Error('Incorrect network selected')
   }
 
-  // const Facet = await ethers.getContractFactory('contracts/Aavegotchi/facets/ItemsFacet.sol:ItemsFacet')
-  // facet = await Facet.deploy()
-  // await facet.deployed()
-  // console.log('Deployed facet:', facet.address)
+  const Facet = await ethers.getContractFactory('contracts/Aavegotchi/facets/ItemsFacet.sol:ItemsFacet')
+  facet = await Facet.deploy()
+  await facet.deployed()
+  console.log('Deployed facet:', facet.address)
 
-  // const newFuncs = [
-  //   getSelector('function findWearableSets(uint256[] calldata _wearableIds) external view returns (uint256[] memory wearableSetIds_)')
-  // ]
-  // let existingFuncs = getSelectors(facet)
-  // for (const selector of newFuncs) {
-  //   if (!existingFuncs.includes(selector)) {
-  //     throw Error(`Selector ${selector} not found`)
-  //   }
-  // }
-  // existingFuncs = existingFuncs.filter(selector => !newFuncs.includes(selector))
+  const newFuncs = [
+    getSelector('function findWearableSets(uint256[] calldata _wearableIds) external view returns (uint256[] memory wearableSetIds_)')
+  ]
+  let existingFuncs = getSelectors(facet)
+  for (const selector of newFuncs) {
+    if (!existingFuncs.includes(selector)) {
+      throw Error(`Selector ${selector} not found`)
+    }
+  }
+  existingFuncs = existingFuncs.filter(selector => !newFuncs.includes(selector))
 
-  // const FacetCutAction = { Add: 0, Replace: 1, Remove: 2 }
+  const FacetCutAction = { Add: 0, Replace: 1, Remove: 2 }
 
-  // const cut = [
-  //   {
-  //     facetAddress: facet.address,
-  //     action: FacetCutAction.Add,
-  //     functionSelectors: newFuncs
-  //   },
-  //   {
-  //     facetAddress: facet.address,
-  //     action: FacetCutAction.Replace,
-  //     functionSelectors: existingFuncs
-  //   }
-  // ]
-  // console.log(cut)
+  const cut = [
+    {
+      facetAddress: facet.address,
+      action: FacetCutAction.Add,
+      functionSelectors: newFuncs
+    },
+    {
+      facetAddress: facet.address,
+      action: FacetCutAction.Replace,
+      functionSelectors: existingFuncs
+    }
+  ]
+  console.log(cut)
 
-  // const diamondCut = (await ethers.getContractAt('IDiamondCut', diamondAddress)).connect(signer)
-  // let tx
-  // let receipt
+  const diamondCut = (await ethers.getContractAt('IDiamondCut', diamondAddress)).connect(signer)
+  let tx
+  let receipt
 
-  // console.log('Diamond cut')
-  // tx = await diamondCut.diamondCut(cut, ethers.constants.AddressZero, '0x', { gasLimit: 8000000 })
-  // console.log('Diamond cut tx:', tx.hash)
-  // receipt = await tx.wait()
-  // if (!receipt.status) {
-  //   throw Error(`Diamond upgrade failed: ${tx.hash}`)
-  // }
-  // console.log('Completed diamond cut: ', tx.hash)
+  console.log('Diamond cut')
+  tx = await diamondCut.diamondCut(cut, ethers.constants.AddressZero, '0x', { gasLimit: 8000000 })
+  console.log('Diamond cut tx:', tx.hash)
+  receipt = await tx.wait()
+  if (!receipt.status) {
+    throw Error(`Diamond upgrade failed: ${tx.hash}`)
+  }
+  console.log('Completed diamond cut: ', tx.hash)
 
   async function printSets (wearableIds) {
     facet = await ethers.getContractAt('contracts/Aavegotchi/facets/ItemsFacet.sol:ItemsFacet', diamondAddress)
