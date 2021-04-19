6d48c1ef18fa81a2697f3ba7e6db135e6be8c043
Coderdan <coderdannn@gmail.com>
2021-04-19 15:48:36 +0800

    prepping script to add miami wearable and send to aavegotchi.eth

--- scripts/addMiamiShirt.js
+++ scripts/addMiamiShirt.js
@@ -1,90 +1,169 @@
 /* global ethers hre */
 /* eslint-disable  prefer-const */
 
-//const { LedgerSigner } = require('@ethersproject/hardware-wallets')
+const { LedgerSigner } = require('@ethersproject/hardware-wallets')
+const { itemTypes } = require('./miamiShirtItemType')
+const { wearablesSvgs, sleevesSvgs } = require('../svgs/miamiShirtWearables')
 
-const itemTypes = [
-  {
-    svgId: 162,
-    name: 'Miami Shirt',
-    setId: [],
-    author: 'xibot',
-    description: '',
-    dimensions: { x: 13, y: 6, width: 38, height: 27 },
-    allowedCollaterals: [],
-    minLevel: 1,
-    ghstPrice: 5,
-    maxQuantity: 1000,
-    traitModifiers: [1, 0, 0, 0, 0, 0],
-    canPurchaseWithGhst: false,
-    slotPositions: 'body',
-    category: 0,
-    canBeTransferred: true,
-    totalQuantity: 0,
-    experienceBonus: 0,
-    kinshipBonus: 0
-  }
-]
+let signer
+const diamondAddress = '0x86935F11C86623deC8a25696E1C19a8659CbF95d'
+const gasLimit = 15000000
 
-function stringToSlotPositions (str) {
-  if (str.length === 0) return [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
-  // Slot 0 Body
-  else if (str === 'body') return [true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
-
-  // Slot 1 Face
-  else if (str === 'face') return [false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
-
-  // Slot 2 Eyes
-  else if (str === 'eyes') return [false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false]
-
-  // Slot 3 Head
-  else if (str === 'head') return [false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false]
-
-  // Slot 4/5 Either hand
-  else if (str === 'hands') return [false, false, false, false, true, true, false, false, false, false, false, false, false, false, false, false]
-
-  // Slot 4 Left hand
-  else if (str === 'handLeft') return [false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false]
-
-  // Slot 5 Right Hand
-  else if (str === 'handRight') return [false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false]
-
-  // Slot 6 Pet
-  else if (str === 'pet') return [false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false]
-
-  // Slot 7 Background
-  else if (str === 'background') return [false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false]
-
-  else {
-    throw (Error('Wrong slot string: ' + str))
-  }
-}
-
-function calculateRarityScoreModifier (maxQuantity) {
-  if (maxQuantity >= 1000) return 1
-  if (maxQuantity >= 500) return 2
-  if (maxQuantity >= 250) return 5
-  if (maxQuantity >= 100) return 10
-  if (maxQuantity >= 10) return 20
-  if (maxQuantity >= 1) return 50
-  return 0
-}
-
-function getItemTypes () {
-  const result = []
-  for (const itemType of itemTypes) {
-    itemType.ghstPrice = ethers.utils.parseEther(itemType.ghstPrice.toString())
-    itemType.slotPositions = stringToSlotPositions(itemType.slotPositions)
-    if (itemType.dimensions === '' || itemType.dimensions === 0) {
-      itemType.dimensions = { x: 0, y: 0, width: 0, height: 0 }
+async function uploadSvgs (svgs) {
+  let svgFacet = (await ethers.getContractAt('SvgFacet', diamondAddress)).connect(signer)
+  function setupSvg (...svgData) {
+    const svgTypesAndSizes = []
+    const svgItems = []
+    for (const [svgType, svg] of svgData) {
+      svgItems.push(svg.join(''))
+      svgTypesAndSizes.push([ethers.utils.formatBytes32String(svgType), svg.map(value => value.length)])
     }
-    itemType.rarityScoreModifier = calculateRarityScoreModifier(itemType.maxQuantity)
-    if (!Array.isArray(itemType.allowedCollaterals)) {
-      throw Error('Is not array.')
-    }
-    result.push(itemType)
+    return [svgItems.join(''), svgTypesAndSizes]
   }
-  return result
+
+  // eslint-disable-next-line no-unused-vars
+  function printSizeInfo (svgTypesAndSizes) {
+    console.log('------------- SVG Size Info ---------------')
+    let sizes = 0
+    for (const [svgType, size] of svgTypesAndSizes) {
+      console.log(ethers.utils.parseBytes32String(svgType) + ':' + size)
+      for (const nextSize of size) {
+        sizes += nextSize
+      }
+    }
+    console.log('Total sizes:' + sizes)
+  }
+
+  console.log('Uploading ', svgs.length, ' svgs')
+  let svg, svgTypesAndSizes
+  console.log('Number of svg:' + svgs.length)
+  let svgItemsStart = 0
+  let svgItemsEnd = 0
+  while (true) {
+    let itemsSize = 0
+    while (true) {
+      if (svgItemsEnd === svgs.length) {
+        break
+      }
+      itemsSize += svgs[svgItemsEnd].length
+      if (itemsSize > 24576) {
+        break
+      }
+      svgItemsEnd++
+    }
+    ;[svg, svgTypesAndSizes] = setupSvg(
+      ['wearables', svgs.slice(svgItemsStart, svgItemsEnd)]
+    )
+    console.log(`Uploading ${svgItemsStart} to ${svgItemsEnd} wearable SVGs`)
+    printSizeInfo(svgTypesAndSizes)
+    let tx = await svgFacet.storeSvg(svg, svgTypesAndSizes, { gasLimit: gasLimit })
+    let receipt = await tx.wait()
+    if (!receipt.status) {
+      throw Error(`Error:: ${tx.hash}`)
+    }
+    console.log(svgItemsEnd, svg.length)
+    if (svgItemsEnd === svgs.length) {
+      break
+    }
+    svgItemsStart = svgItemsEnd
+  }
 }
 
-exports.itemTypes = getItemTypes()
+async function main () {
+
+
+
+  let owner = await (await ethers.getContractAt('OwnershipFacet', diamondAddress)).owner()
+  const testing = ['hardhat', 'localhost'].includes(hre.network.name)
+  if (testing) {
+
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [{
+        forking: {
+          jsonRpcUrl: process.env.MATIC_URL,
+        }
+      }]
+    })
+
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [owner]
+    })
+    signer = await ethers.provider.getSigner(owner)
+  } else if (hre.network.name === 'matic') {
+    signer = new LedgerSigner(ethers.provider)
+  } else {
+    throw Error('Incorrect network selected')
+  }
+  let tx
+  let receipt
+
+  let daoFacet = (await ethers.getContractAt('DAOFacet', diamondAddress)).connect(signer)
+  let svgFacet = (await ethers.getContractAt('SvgFacet', diamondAddress)).connect(signer)
+
+  let itemsFacet = await ethers.getContractAt('contracts/Aavegotchi/facets/ItemsFacet.sol:ItemsFacet',diamondAddress)
+
+  console.log('Adding items', 0, 'to', itemTypes.length)
+  tx = await daoFacet.addItemTypes(itemTypes, { gasLimit: gasLimit })
+  receipt = await tx.wait()
+  if (!receipt.status) {
+    throw Error(`Error:: ${tx.hash}`)
+  }
+  console.log('Items were added:', tx.hash)
+
+
+   let item = await itemsFacet.getItemType(162)
+   console.log('Item:', item)
+
+  await uploadSvgs(wearablesSvgs)
+  await uploadSvgs(sleevesSvgs.map(value => value.svg))
+
+  let sleevesSvgId = 23
+  let sleeves = []
+  for (const sleeve of sleevesSvgs) {
+    sleeves.push(
+      {
+        sleeveId: sleevesSvgId,
+        wearableId: sleeve.id
+      }
+    )
+    sleevesSvgId++
+  }
+  console.log('Associating sleeves svgs with body wearable svgs.')
+  tx = await svgFacet.setSleeves(sleeves, { gasLimit: gasLimit })
+  receipt = await tx.wait()
+  if (!receipt.status) {
+    throw Error(`Error:: ${tx.hash}`)
+  }
+  console.log('Sleeves associated:', tx.hash)
+
+
+  const finalSVG = await svgFacet.getItemSvg("162")
+
+  console.log('final svg:',finalSVG)
+
+  // deploy raffle:
+  console.log('Mint items to aavegotchi.eth')
+  let mintAddress = "0x027Ffd3c119567e85998f4E6B9c3d83D5702660c"
+
+  
+
+
+  tx = await daoFacet.mintItems(mintAddress, [162], [1000])
+  receipt = await tx.wait()
+  if (!receipt.status) {
+    throw Error(`Error:: ${tx.hash}`)
+  }
+  console.log('Prize items minted:', tx.hash)
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
