a0b7755bf32880ac469f0cb81dcfde0a1131c0d5
Mark Watson <markjwatson86@gmail.com>
2021-04-23 14:30:57 -0700

    testing upgrade

--- scripts/upgrades/upgrade-escrowTransfer.js
+++ scripts/upgrades/upgrade-escrowTransfer.js
@@ -96,11 +96,13 @@ async function main () {
   }
 }
 
+if(require.main === module){
   main()
     .then(() => process.exit(0))
       .catch(error => {
         console.error(error)
         process.exit(1)
     });
+  }
 
     exports.escrowProject = main
