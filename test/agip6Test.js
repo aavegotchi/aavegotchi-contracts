a4bda96d7be4285f2162e5c05a0af893c0d690fc
Mark Watson <markjwatson86@gmail.com>
2021-04-29 21:32:33 -0700

    test update

--- test/agip6Test.js
+++ test/agip6Test.js
@@ -9,15 +9,17 @@ describe("Fees", async function(){
 
   let aavegotchiDiamondAddress,
       maticGhstAddress,
+      ghstERC20,
       erc721Facet,
       erc1155Facet,
       owner,
+      buyer,
       aavegotchiOwner,
       ghstWhale;
 
   before(async function() {
     aavegotchiDiamondAddress = '0x86935F11C86623deC8a25696E1C19a8659CbF95d';
-    ghstWhale = '0xA02d547512Bb90002807499F05495Fe9C4C3943f';
+    ghstWhale = '0x20eC02894D748C59c01B6bF08FE283D7bB75A5d2';
     maticGhstAddress = '0x385Eeac5cB85A38A9a07A70c73e0a3271CfB54A7';
 
     await agip6Project();
@@ -25,31 +27,35 @@ describe("Fees", async function(){
     erc721Facet = await ethers.getContractAt('ERC721MarketplaceFacet', aavegotchiDiamondAddress);
     erc1155Facet = await ethers.getContractAt('ERC1155MarketplaceFacet', aavegotchiDiamondAddress);
 
+
+  });
+
+  it.only("Should check fees amount", async function(){
     await hre.network.provider.request({
       method: 'hardhat_impersonateAccount',
       params: [ghstWhale]
     });
 
-  });
-
-  it.only("Should check fees amount", async function(){
-
-    const ghstContract = await ethers.getContractAt('ERC20Token', maticGhstAddress);
+    ghstERC20 = await ethers.getContractAt('ERC20Token', maticGhstAddress);
 
     buyer = await ethers.getSigner(ghstWhale);
-    const connectedGhstContract = await ghstContract.connect(buyer);
+
+    const connectedGhstContract = await ghstERC20.connect(buyer);
     const connectedERC721Facet = await erc721Facet.connect(buyer);
 
     let beforeBalance = await connectedGhstContract.balanceOf(ghstWhale)
     console.log('before balance:', beforeBalance.toString())
 
-      // await connectedGhstContract.approve(aavegotchiDiamondAddress, ethers.constants.MaxUint256);
+    await connectedGhstContract.approve(aavegotchiDiamondAddress, ethers.constants.MaxUint256);
+    let allowance = await connectedGhstContract.allowance(ghstWhale, aavegotchiDiamondAddress);
+    console.log("Allowance: ", allowance.toString());
 
-    const erc721Listing = await connectedERC721Facet.getERC721Listings(2, 'listed', 5);
+    const erc721Listing = await erc721Facet.getERC721Listings(2, 'listed', 5);
     console.log("ERC721 Listings: ", erc721Listing.toString());
 
-    // const execute721Listing = await connectedERC721Facet.executeERC721Listing(76327);
-    // console.log("ERC721 Executed: ", execute721Listing);
+    //may need to find different listingID from erc721Listing if fails
+    const execute721Listing = await erc721Facet.executeERC721Listing(76705);
+    console.log("ERC721 Executed: ", execute721Listing);
 
   });
 });
