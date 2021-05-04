46849a966f1ef472ae7c48bfa0d9eac26227e0a6
Coderdan <coderdannn@gmail.com>
2021-05-04 14:56:19 +0800

    aapprentices airdrop

---
+++ data/airdrops/aapprentices.tsx
@@ -0,0 +1,232 @@
+const aapprenticeAddresses = [
+  "0x115b65464043e9d0ad7422f95d1398b593c0efd3",
+  "0x9e42fd37e21c0ed235fa31d2f49a7fbdea961f5c",
+  "0xf1fced5b0475a935b49b95786adbda2d40794d2d",
+  "0x4eb172821b5bc013269c4f6f6204f092d44197ec",
+  "0xfeac872a93df8939df3face650945fbf3ea705e9",
+  "0x399158992e64656d4fe05b11fd7ef4adcad1de59",
+  "0xafd5ec1a5fe09e6597de92c34a423d7c35864023",
+  "0xed46a40c088d11546eb4811e565e88a03ae8a07c",
+  "0x6e0fc44cce1b49323185138217649b5e8996a159",
+  "0x9796c54f15e845a6778b37a15240a96452215c02",
+  "0xdcd050fad8eaef5dc11bd25e92014d21dcada74d",
+  "0xc6291442efe2634306b31f24c8238a702fec85a0",
+  "0x40ed4254b02ee508b4e894b5ebdd7bcf9d88a716",
+  "0x0db5e7924d87d2ec27876882e7af911a58050dd1",
+  "0x5b9de7c6b24f1baf04b3bdde9f656f5550f6c9eb",
+  "0x3c6d73475d8a64cec5b5170853ab38ccf51eb130",
+  "0xcef4d05050a9b342991fa7b06b27df1ae03c7d1b",
+  "0x680f63f7948702239fdfd5dcfa0e65498b431cb5",
+];
+
+const aapprenticeGotchisOwned = {
+  data: {
+    users: [
+      {
+        gotchisOwned: [
+          {
+            id: "4802",
+          },
+          {
+            id: "4803",
+          },
+          {
+            id: "4806",
+          },
+        ],
+        id: "0x0db5e7924d87d2ec27876882e7af911a58050dd1",
+      },
+      {
+        gotchisOwned: [
+          {
+            id: "8812",
+          },
+          {
+            id: "8963",
+          },
+        ],
+        id: "0x3c6d73475d8a64cec5b5170853ab38ccf51eb130",
+      },
+      {
+        gotchisOwned: [
+          {
+            id: "110",
+          },
+          {
+            id: "1218",
+          },
+          {
+            id: "2368",
+          },
+          {
+            id: "3711",
+          },
+          {
+            id: "5419",
+          },
+          {
+            id: "6786",
+          },
+        ],
+        id: "0x4eb172821b5bc013269c4f6f6204f092d44197ec",
+      },
+      {
+        gotchisOwned: [
+          {
+            id: "7584",
+          },
+        ],
+        id: "0x680f63f7948702239fdfd5dcfa0e65498b431cb5",
+      },
+      {
+        gotchisOwned: [
+          {
+            id: "6088",
+          },
+        ],
+        id: "0xafd5ec1a5fe09e6597de92c34a423d7c35864023",
+      },
+      {
+        gotchisOwned: [
+          {
+            id: "6268",
+          },
+        ],
+        id: "0xc6291442efe2634306b31f24c8238a702fec85a0",
+      },
+      {
+        gotchisOwned: [
+          {
+            id: "4964",
+          },
+        ],
+        id: "0xcef4d05050a9b342991fa7b06b27df1ae03c7d1b",
+      },
+      {
+        gotchisOwned: [
+          {
+            id: "6094",
+          },
+        ],
+        id: "0xdcd050fad8eaef5dc11bd25e92014d21dcada74d",
+      },
+      {
+        gotchisOwned: [
+          {
+            id: "7467",
+          },
+          {
+            id: "9891",
+          },
+        ],
+        id: "0xed46a40c088d11546eb4811e565e88a03ae8a07c",
+      },
+      {
+        gotchisOwned: [
+          {
+            id: "1165",
+          },
+          {
+            id: "2398",
+          },
+          {
+            id: "2475",
+          },
+          {
+            id: "2558",
+          },
+          {
+            id: "2835",
+          },
+          {
+            id: "3040",
+          },
+          {
+            id: "3676",
+          },
+          {
+            id: "4538",
+          },
+          {
+            id: "4827",
+          },
+          {
+            id: "5387",
+          },
+          {
+            id: "5661",
+          },
+          {
+            id: "5764",
+          },
+          {
+            id: "5875",
+          },
+          {
+            id: "5942",
+          },
+          {
+            id: "625",
+          },
+          {
+            id: "6484",
+          },
+          {
+            id: "6536",
+          },
+          {
+            id: "6798",
+          },
+          {
+            id: "6864",
+          },
+          {
+            id: "7086",
+          },
+          {
+            id: "8024",
+          },
+          {
+            id: "8224",
+          },
+          {
+            id: "8235",
+          },
+          {
+            id: "8798",
+          },
+          {
+            id: "8859",
+          },
+          {
+            id: "9372",
+          },
+          {
+            id: "9623",
+          },
+          {
+            id: "9726",
+          },
+          {
+            id: "9772",
+          },
+        ],
+        id: "0xf1fced5b0475a935b49b95786adbda2d40794d2d",
+      },
+      {
+        gotchisOwned: [
+          {
+            id: "6769",
+          },
+          {
+            id: "9071",
+          },
+        ],
+        id: "0xfeac872a93df8939df3face650945fbf3ea705e9",
+      },
+    ],
+  },
+};
+
+exports.aapprenticeAddresses = aapprenticeAddresses;
+exports.aapprenticeGotchisOwned = aapprenticeGotchisOwned;
