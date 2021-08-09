92cd45bc760bd52236f3e8981ee100f25cac5362
Timidan <akinnusotutemitayodaniel@gmail.com>
2021-08-09 09:32:59 +0000

    added buyPortals function back in

---
+++ svgs/openPortalSvg.js
@@ -0,0 +1,26 @@
+/* eslint-disable  prefer-const */
+
+const fs = require("fs");
+
+const portals = [];
+portal("openPortal");
+
+function stripSvg(svg) {
+  // removes svg tag
+  if (svg.includes("viewBox")) {
+    svg = svg.slice(svg.indexOf(">") + 1);
+    svg = svg.replace("</svg>", "");
+  }
+  return svg;
+}
+
+function readSvg(name) {
+  return stripSvg(fs.readFileSync(`./svgs/${name}.svg`, "utf8"));
+}
+
+function portal(name) {
+  const svg = readSvg(name);
+  portals.push(svg);
+}
+
+exports.openPortalSvgs = portals;
