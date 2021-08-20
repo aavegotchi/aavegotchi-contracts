169bef2dc2784cc51008b6c31e3ddce27a7c9528
Coderdan <coderdannn@gmail.com>
2021-08-20 13:40:38 +0800

    deploy mintPortals upgrade

--- scripts/upgrades/upgrade-mintPortal.js
+++ scripts/upgrades/upgrade-mintPortal.js
@@ -106,13 +106,11 @@ async function main() {
   }
 }
 
-/*
 main()
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
     process.exit(1);
   });
-  */
 
 exports.mintPortal = main;
