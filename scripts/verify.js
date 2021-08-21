6e79c603aea7aa3ee85067f9d093449c1ddd87b4
Coderdan <coderdannn@gmail.com>
2021-08-21 16:29:04 +0800

    verified ShopFacet and SvgFacet

--- scripts/verify.js
+++ scripts/verify.js
@@ -1,14 +1,20 @@
-const { run } = require("hardhat")
+const { run } = require("hardhat");
 
-async function verify(address, facet) {
-  // const address = '0xfa7a3bb12848A7856Dd2769Cd763310096c053F1'; // deployed address
-  // const facet = 'StakingFacet'; // name of facet
-  await run('verifyFacet', {
+async function verify() {
+  const address = "0xEA849a2B683Fed2BbE49610b7A01607fb386DE0A"; // deployed address
+  const facet = "SvgFacet"; // name of facet
+  await run("verifyFacet", {
     apikey: process.env.POLYGON_API_KEY,
     contract: address,
-    facet
+    facet,
   });
 }
 
-// verify('0x691a713894403bF3b8a8A871BAB0D755D4b15096', 'AavegotchiFacet');
-exports.VerifyFacet = verify
+verify()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+exports.VerifyFacet = verify;
