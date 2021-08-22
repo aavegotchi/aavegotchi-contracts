f6a4804c1c766c743dde25fd61f735b0789bb809
Temitayo Daniel <akinnusotutemitayodaniel@gmail.com>
2021-08-22 07:34:03 +0100

    upgrade script not complete

--- scripts/createHaunt.js
+++ scripts/createHaunt.js
@@ -1,23 +1,86 @@
 /* global ethers hre */
-
-async function main () {
-  const aavegotchiDiamondAddress = '0x86935F11C86623deC8a25696E1C19a8659CbF95d'
-  const hauntSize = 10000
-  const price = ethers.utils.parseEther('100')
-  const daoFacet = await ethers.getContractAt('DAOFacet', aavegotchiDiamondAddress)
-  const tx = await daoFacet.createHaunt(hauntSize, price, '0x000000', { gasLimit: 5000000 })
-  const receipt = await tx.wait()
-  if (!receipt.status) {
-    throw Error(`Error creating haunt: ${tx.hash}`)
+const { getH2Collaterals } = require("./collateralTypesHaunt2.js");
+const collateralsSvgs = require("../svgs/collateralsH2.js");
+const { eyeShapeSvgs } = require("../../svgs/eyeShapesH2.js");
+async function main() {
+  let svg, svgTypesAndSizes;
+  function setupSvg(...svgData) {
+    const svgTypesAndSizes = [];
+    const svgItems = [];
+    for (const [svgType, svg] of svgData) {
+      svgItems.push(svg.join(""));
+      svgTypesAndSizes.push([
+        ethers.utils.formatBytes32String(svgType),
+        svg.map((value) => value.length),
+      ]);
+    }
+    return [svgItems.join(""), svgTypesAndSizes];
   }
-  console.log('Haunt created:', tx.hash)
+
+  // eslint-disable-next-line no-unused-vars
+  function printSizeInfo(svgTypesAndSizes) {
+    console.log("------------- SVG Size Info ---------------");
+    let sizes = 0;
+    for (const [svgType, size] of svgTypesAndSizes) {
+      console.log(ethers.utils.parseBytes32String(svgType) + ":" + size);
+      for (const nextSize of size) {
+        sizes += nextSize;
+      }
+    }
+    console.log("Total sizes:" + sizes);
+  }
+
+  const aavegotchiDiamondAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
+  const hauntSize = 10000;
+  const price = ethers.utils.parseEther("100");
+  const daoFacet = await ethers.getContractAt(
+    "DAOFacet",
+    aavegotchiDiamondAddress
+  );
+  const _hauntMaxSize = 15000;
+  const portalPrice = 0; //GBM
+  const _bodyColor = 0x1f; //test color
+  const _collateralTypes = getH2Collaterals();
+  const H2Svgs = collateralsSvgs;
+
+  [svg, svgTypesAndSizes] = setupSvg(["collaterals", collateralsSvgs]);
+
+  const _collTypes = [];
+  _collTypes.push(svg, svgTypesAndSizes);
+
+  const eyeShapes = eyeShapeSvgs;
+  [svg, svgTypesAndSizes] = setupSvg(["eyeShapesH2", eyeShapeSvgs]);
+  const eyeShapesTypes = [];
+  eyeShapesTypes.push(svg, svgTypesAndSizes);
+
+  const totalPayload = [];
+
+  totalPayload.push(
+    _hauntMaxSize,
+    portalPrice,
+    _bodyColor,
+    _collateralTypes,
+    H2Svgs,
+    _collTypes,
+    eyeShapeSvgs,
+    eyeShapesTypes
+  );
+
+  const tx = await daoFacet.createHauntPayload(totalPayload, {
+    gasLimit: 5000000,
+  });
+  const receipt = await tx.wait();
+  if (!receipt.status) {
+    throw Error(`Error creating haunt: ${tx.hash}`);
+  }
+  console.log("Haunt created:", tx.hash);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
   .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error)
-    process.exit(1)
-  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/upgrades/upgrade-hauntPayload.js
@@ -0,0 +1,111 @@
+const { LedgerSigner } = require("@ethersproject/hardware-wallets");
+const { sendToMultisig } = require("../libraries/multisig/multisig.js");
+
+function getSelectors(contract) {
+  const signatures = Object.keys(contract.interface.functions);
+  const selectors = signatures.reduce((acc, val) => {
+    if (val !== "init(bytes)") {
+      acc.push(contract.interface.getSighash(val));
+    }
+    return acc;
+  }, []);
+  return selectors;
+}
+
+function getSelector(func) {
+  const abiInterface = new ethers.utils.Interface([func]);
+  return abiInterface.getSighash(ethers.utils.Fragment.from(func));
+}
+
+async function main() {
+  const gasPrice = 2000000000;
+  const diamondAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
+  let signer;
+  const owner = await (
+    await ethers.getContractAt("OwnershipFacet", diamondAddress)
+  ).owner();
+  const testing = ["hardhat", "localhost"].includes(hre.network.name);
+
+  if (testing) {
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [owner],
+    });
+    signer = await ethers.getSigner(owner);
+  } else if (hre.network.name === "matic") {
+    signer = new LedgerSigner(ethers.provider);
+  } else {
+    throw Error("Incorrect network selected");
+  }
+
+  const DAOFacet = await ethers.getContractFactory("DAOFacet");
+  let daoFacet = await DAOFacet.deploy({ gasPrice: gasPrice });
+  await daoFacet.deployed();
+  console.log("Deployed DAOFacet");
+
+  const newDaoFunc = [
+    getSelector(
+      "function CreateHauntWithPayload(uint24 _hauntMaxSize,uint96 _portalPrice,bytes3 _bodyColor,tuple(address collateralType, tuple(int16[6] modifiers, bytes3 primaryColor, bytes3 secondaryColor, bytes3 cheekColor, uint8 svgId, uint8 eyeShapeSvgId, uint16 conversionRate, bool delisted) collateralTypeInfo)[] _collateralTypes,string[] _collateralSvgs,tuple(bytes32 svgType,uint256[] sizes)[][] _collateralTypesAndSizes,string[] _eyeShapeSvgs,tuple(bytes32 svgType,uint256[] sizes)[][] _eyeShapeTypesAndSizes) external"
+    ),
+  ];
+
+  let existingDaoFuncs = getSelectors(daoFacet);
+  for (const selector of newDaoFunc) {
+    if (!existingDaoFuncs.includes(selector)) {
+      throw Error(`Selector ${selector} not found`);
+    }
+  }
+
+  existingDaoFuncs = existingDaoFuncs.filter(
+    (selector) => !newDaoFunc.includes(selector)
+  );
+
+  const FacetCutAction = { Add: 0, Replace: 1, Remove: 2 };
+
+  const cut = [
+    {
+      facetAddress: daoFacet.address,
+      action: FacetCutAction.Add,
+      functionSelectors: newDaoFunc,
+    },
+  ];
+
+  console.log(cut);
+
+  const diamondCut = (
+    await ethers.getContractAt("IDiamondCut", diamondAddress)
+  ).connect(signer);
+  let tx;
+  let receipt;
+
+  if (testing) {
+    console.log("Diamond cut");
+    tx = await diamondCut.diamondCut(cut, ethers.constants.AddressZero, "0x", {
+      gasLimit: 8000000,
+    });
+    console.log("Diamond cut tx:", tx.hash);
+    receipt = await tx.wait();
+    if (!receipt.status) {
+      throw Error(`Diamond upgrade failed: ${tx.hash}`);
+    }
+    console.log("Completed diamond cut: ", tx.hash);
+  } else {
+    console.log("Diamond cut");
+    tx = await diamondCut.populateTransaction.diamondCut(
+      cut,
+      ethers.constants.AddressZero,
+      "0x",
+      { gasLimit: 800000 }
+    );
+    await sendToMultisig(process.env.DIAMOND_UPGRADER, signer, tx);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+exports.addPayload = main;
