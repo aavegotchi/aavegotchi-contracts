cc7e1d703c697748aaebd959831bb9989b8fa474
Temitayo Daniel <akinnusotutemitayodaniel@gmail.com>
2021-08-23 05:20:56 +0100

    svg.join is not a function

--- scripts/createHaunt.js
+++ scripts/createHaunt.js
@@ -1,8 +1,30 @@
 /* global ethers hre */
 const { getH2Collaterals } = require("./collateralTypesHaunt2.js");
 const collateralsSvgs = require("../svgs/collateralsH2.js");
-const { eyeShapeSvgs } = require("../../svgs/eyeShapesH2.js");
+const { eyeShapeSvgs } = require("../svgs/eyeShapesH2.js");
+const { addPayload } = require("./upgrades/upgrade-hauntPayload.js");
+
+let signer, daoFacet;
 async function main() {
+  console.log("upgrading");
+  await addPayload();
+  const aavegotchiDiamondAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
+  const itemManager = "0xa370f2ADd2A9Fba8759147995d6A0641F8d7C119";
+
+  const testing = ["hardhat", "localhost"].includes(hre.network.name);
+  if (testing) {
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [itemManager],
+    });
+
+    signer = await ethers.provider.getSigner(itemManager);
+  } else if (hre.network.name === "matic") {
+    signer = new LedgerSigner(ethers.provider);
+  } else {
+    throw Error("Incorrect network selected");
+  }
+
   let svg, svgTypesAndSizes;
   function setupSvg(...svgData) {
     const svgTypesAndSizes = [];
@@ -30,13 +52,6 @@ async function main() {
     console.log("Total sizes:" + sizes);
   }
 
-  const aavegotchiDiamondAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
-  const hauntSize = 10000;
-  const price = ethers.utils.parseEther("100");
-  const daoFacet = await ethers.getContractAt(
-    "DAOFacet",
-    aavegotchiDiamondAddress
-  );
   const _hauntMaxSize = 15000;
   const portalPrice = 0; //GBM
   const _bodyColor = 0x1f; //test color
@@ -44,12 +59,13 @@ async function main() {
   const H2Svgs = collateralsSvgs;
 
   [svg, svgTypesAndSizes] = setupSvg(["collaterals", collateralsSvgs]);
-
+  printSizeInfo(svgTypesAndSizes);
   const _collTypes = [];
   _collTypes.push(svg, svgTypesAndSizes);
 
   const eyeShapes = eyeShapeSvgs;
   [svg, svgTypesAndSizes] = setupSvg(["eyeShapesH2", eyeShapeSvgs]);
+  printSizeInfo(svgTypesAndSizes);
   const eyeShapesTypes = [];
   eyeShapesTypes.push(svg, svgTypesAndSizes);
 
@@ -62,18 +78,23 @@ async function main() {
     _collateralTypes,
     H2Svgs,
     _collTypes,
-    eyeShapeSvgs,
+    eyeShapes,
     eyeShapesTypes
   );
+  daoFacet = (
+    await ethers.getContractAt("DAOFacet", aavegotchiDiamondAddress)
+  ).connect(signer);
 
-  const tx = await daoFacet.createHauntPayload(totalPayload, {
-    gasLimit: 5000000,
-  });
-  const receipt = await tx.wait();
-  if (!receipt.status) {
-    throw Error(`Error creating haunt: ${tx.hash}`);
+  if (testing) {
+    const tx = await daoFacet.createHauntPayload(totalPayload, {
+      gasLimit: 15000000,
+    });
+    const receipt = await tx.wait();
+    if (!receipt.status) {
+      throw Error(`Error creating haunt: ${tx.hash}`);
+    }
+    console.log("Haunt created:", tx.hash);
   }
-  console.log("Haunt created:", tx.hash);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- scripts/upgrades/upgrade-hauntPayload.js
+++ scripts/upgrades/upgrade-hauntPayload.js
@@ -102,11 +102,11 @@ async function main() {
   }
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+//main()
+//.then(() => process.exit(0))
+//.catch((error) => {
+// console.error(error);
+//  process.exit(1);
+//});
 
 exports.addPayload = main;
