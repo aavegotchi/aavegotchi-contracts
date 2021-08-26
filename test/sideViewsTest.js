2145704b9921339ca0854c447ebb1d3559d5f679
Mark Watson <markjwatson86@gmail.com>
2021-08-26 14:34:22 -0700

    update dimension

--- test/sideViewsTest.js
+++ test/sideViewsTest.js
@@ -32,7 +32,7 @@ describe("Side Views", async function () {
     // const svgs = await svgViewsFacet.getAavegotchiSideSvgs(7623);
     // console.log('svgs:',svgs);
     const numTraits1 = [99, 99, 99, 99, 0, 0];
-    const wearables1 = [19, 0, 0, 0, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const wearables1 = [15, 13, 0, 4, 3, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
     const frontPreview = await svgFacet.previewAavegotchi("1", "0xE0b22E0037B130A9F56bBb537684E6fA18192341", numTraits1, wearables1);
     console.log("Front Preview: ", frontPreview);
