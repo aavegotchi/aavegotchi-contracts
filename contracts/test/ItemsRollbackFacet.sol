3ec39c21557c9c22e4062d2b8b8742b2dfb0101d
orionstardust <justinono1990@outlook.com>
2021-09-02 02:43:51 +0000

    Add new contract and deploy script for fix burned wearables

---
+++ contracts/test/ItemsRollbackFacet.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.1;
+
+import {LibItems} from "../Aavegotchi/libraries/LibItems.sol";
+
+contract ItemsRollbackFacet {
+    function rollback() external {
+        address aavegotchiDiamond = 0x86935F11C86623deC8a25696E1C19a8659CbF95d;
+        address userAddress = 0xed3BBbe2e3eacE311a94b059508Bbdda9149AB23;
+        uint256 tokenId = 1172;
+        uint256[2] memory wearableIds = [uint256(235), uint256(236)];
+        uint256[2] memory wearableCounts = [uint256(1), uint256(1)];
+
+        for (uint256 i; i < wearableIds.length; i++) {
+            LibItems.removeFromParent(aavegotchiDiamond, tokenId, wearableIds[i], wearableCounts[i]);
+            LibItems.addToOwner(userAddress, wearableIds[i], wearableCounts[i]);
+        }
+    }
+}
---
+++ scripts/upgrades/upgrade-itemsRollback.js
@@ -0,0 +1,68 @@
+/* global ethers */
+const { sendToMultisig } = require("../libraries/multisig/multisig.js");
+const { LedgerSigner } = require("@ethersproject/hardware-wallets");
+
+async function main() {
+  const gasPrice = 2000000000;
+  const diamondAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
+  let signer;
+  let facet;
+  let tx;
+  const testing = ["hardhat", "localhost"].includes(hre.network.name);
+
+  if (testing) {
+    const owner = await (
+      await ethers.getContractAt("OwnershipFacet", diamondAddress)
+    ).owner();
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [owner]
+    });
+    signer = await ethers.provider.getSigner(owner);
+  } else if (hre.network.name === "matic") {
+    signer = new LedgerSigner(ethers.provider);
+  } else {
+    throw Error("Incorrect network selected");
+  }
+
+  const facetFactory = await ethers.getContractFactory("ItemsRollbackFacet");
+  facet = await facetFactory.deploy({
+    gasPrice: gasPrice
+  });
+  await facet.deployed();
+  console.log("ItemsRollbackFacet deployed:", facet.address);
+
+  let functionCall = facet.interface.encodeFunctionData("rollback");
+  const diamondCut = (
+    await ethers.getContractAt("IDiamondCut", diamondAddress)
+  ).connect(signer);
+  if (testing) {
+    tx = await diamondCut.diamondCut([], facet.address, functionCall, { gasLimit: 20000000 });
+    console.log("Diamond cut tx:", tx.hash);
+    const receipt = await tx.wait();
+    if (!receipt.status) {
+      throw Error(`Diamond upgrade failed: ${tx.hash}`);
+    }
+  } else {
+    tx = await diamondCut.populateTransaction.diamondCut([], facet.address, functionCall, { gasLimit: 800000 });
+    console.log("tx:", tx);
+    await sendToMultisig(process.env.DIAMOND_UPGRADER, signer, tx);
+    console.log("Sent to multisig");
+  }
+
+  // test
+  const itemsFacet = await ethers.getContractAt("contracts/Aavegotchi/facets/ItemsFacet.sol:ItemsFacet", diamondAddress);
+  let bals = await itemsFacet.itemBalancesOfToken(diamondAddress, 1172);
+  console.log(bals);
+}
+
+if (require.main === module) {
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
+
+exports.upgradeItemsRollback = main;
