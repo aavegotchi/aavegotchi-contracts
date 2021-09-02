9ff9d53ffc69e7fec3da6aa02d837e03a76fd7d0
orionstardust <justinono1990@outlook.com>
2021-09-02 02:58:04 +0000

    Update contract for all wearables

--- contracts/test/ItemsRollbackFacet.sol
+++ contracts/test/ItemsRollbackFacet.sol
@@ -8,8 +8,8 @@ contract ItemsRollbackFacet {
         address aavegotchiDiamond = 0x86935F11C86623deC8a25696E1C19a8659CbF95d;
         address userAddress = 0xed3BBbe2e3eacE311a94b059508Bbdda9149AB23;
         uint256 tokenId = 1172;
-        uint256[2] memory wearableIds = [uint256(235), uint256(236)];
-        uint256[2] memory wearableCounts = [uint256(1), uint256(1)];
+        uint256[4] memory wearableIds = [uint256(143), uint256(197), uint256(235), uint256(236)];
+        uint256[4] memory wearableCounts = [uint256(2), uint256(1), uint256(5), uint256(10)];
 
         for (uint256 i; i < wearableIds.length; i++) {
             LibItems.removeFromParent(aavegotchiDiamond, tokenId, wearableIds[i], wearableCounts[i]);
