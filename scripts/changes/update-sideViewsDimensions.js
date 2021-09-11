084a864e8d4484f58e716b7ebb70c7ae28315fc1
Coderdan <coderdannn@gmail.com>
2021-09-11 11:51:33 +0800

    -add gasPrice -use accounts[0] signer instead of ledger

--- scripts/changes/update-sideViewsDimensions.js
+++ scripts/changes/update-sideViewsDimensions.js
@@ -1,10 +1,9 @@
-
 /* global ethers hre */
 /* eslint prefer-const: "off" */
 
-const { LedgerSigner } = require('@ethersproject/hardware-wallets')
+const { LedgerSigner } = require("@ethersproject/hardware-wallets");
 
-const { aavegotchiSvgs } = require('../../svgs/aavegotchi-side.js')
+const { aavegotchiSvgs } = require("../../svgs/aavegotchi-side.js");
 
 const {
   sideViewDimensions1,
@@ -14,124 +13,148 @@ const {
   sideViewDimensions5,
   sideViewDimensions6,
   sideViewDimensions7,
-  sideViewDimensions8
-} = require('../../svgs/sideViewDimensions.js')
+  sideViewDimensions8,
+} = require("../../svgs/sideViewDimensions.js");
 
+const gasPrice = 70000000000;
 
-async function main () {
-  const diamondAddress = '0x86935F11C86623deC8a25696E1C19a8659CbF95d'
-  let account1Signer
-  let account1Address
-  let signer
-  let facet
-  let owner = await (await ethers.getContractAt('OwnershipFacet', diamondAddress)).owner()
-  const testing = ['hardhat', 'localhost'].includes(hre.network.name)
+async function main() {
+  const diamondAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
+  let account1Signer;
+  let account1Address;
+  let signer;
+  let facet;
+  let owner = await (
+    await ethers.getContractAt("OwnershipFacet", diamondAddress)
+  ).owner();
+  const testing = ["hardhat", "localhost"].includes(hre.network.name);
   if (testing) {
     await hre.network.provider.request({
-      method: 'hardhat_impersonateAccount',
-      params: [owner]
-    })
-    signer = await ethers.getSigner(owner)
-    let dao = await ethers.getContractAt('DAOFacet', diamondAddress, signer)
-    ;[account1Signer] = await ethers.getSigners()
-    account1Address = await account1Signer.getAddress()
-    let tx = await dao.addItemManagers([account1Address])
-    let receipt = await tx.wait()
+      method: "hardhat_impersonateAccount",
+      params: [owner],
+    });
+    signer = await ethers.getSigner(owner);
+    let dao = await ethers.getContractAt("DAOFacet", diamondAddress, signer);
+    [account1Signer] = await ethers.getSigners();
+    account1Address = await account1Signer.getAddress();
+    let tx = await dao.addItemManagers([account1Address]);
+    let receipt = await tx.wait();
     if (!receipt.status) {
-      throw Error(`Error:: ${tx.hash}`)
+      throw Error(`Error:: ${tx.hash}`);
     }
-    console.log('assigned', account1Address, 'as item manager')
-  } else if (hre.network.name === 'matic') {
-    signer = new LedgerSigner(ethers.provider)
+    console.log("assigned", account1Address, "as item manager");
+  } else if (hre.network.name === "matic") {
+    const accounts = await ethers.getSigners();
+    signer = accounts[0];
   } else {
-    throw Error('Incorrect network selected')
+    throw Error("Incorrect network selected");
   }
 
-  let tx
-  let receipt
-  let itemSigner
+  let tx;
+  let receipt;
+  let itemSigner;
+
   if (testing) {
-    itemSigner = account1Signer
+    itemSigner = account1Signer;
   } else {
-    itemSigner = signer
+    itemSigner = signer;
   }
 
   //dimensions
-  const svgViewsFacet = await ethers.getContractAt('SvgViewsFacet', diamondAddress, itemSigner)
+  const svgViewsFacet = await ethers.getContractAt(
+    "SvgViewsFacet",
+    diamondAddress,
+    itemSigner
+  );
 
   //ID's 1 - 79
-  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions1)
-  receipt = await tx.wait()
+  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions1, {
+    gasPrice: gasPrice,
+  });
+  receipt = await tx.wait();
   if (!receipt.status) {
-    throw Error(`Error:: ${tx.hash}`)
+    throw Error(`Error:: ${tx.hash}`);
   }
-  console.log('Uploaded item side dimensions 1')
+  console.log("Uploaded item side dimensions 1");
 
   //ID's 80 - 118
-  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions2)
-  receipt = await tx.wait()
+  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions2, {
+    gasPrice: gasPrice,
+  });
+  receipt = await tx.wait();
   if (!receipt.status) {
-    throw Error(`Error:: ${tx.hash}`)
+    throw Error(`Error:: ${tx.hash}`);
   }
-  console.log('Uploaded item side dimensions 2')
+  console.log("Uploaded item side dimensions 2");
 
   //ID's 119 - 140
-  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions3)
-  receipt = await tx.wait()
+  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions3, {
+    gasPrice: gasPrice,
+  });
+  receipt = await tx.wait();
   if (!receipt.status) {
-    throw Error(`Error:: ${tx.hash}`)
+    throw Error(`Error:: ${tx.hash}`);
   }
-  console.log('Uploaded item side dimensions 3')
+  console.log("Uploaded item side dimensions 3");
 
   //ID's 141 - 161 & 201 - 204
-  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions4)
-  receipt = await tx.wait()
+  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions4, {
+    gasPrice: gasPrice,
+  });
+  receipt = await tx.wait();
   if (!receipt.status) {
-    throw Error(`Error:: ${tx.hash}`)
+    throw Error(`Error:: ${tx.hash}`);
   }
-  console.log('Uploaded item side dimensions 4')
+  console.log("Uploaded item side dimensions 4");
 
   //ID's 199 - 209
-  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions5)
-  receipt = await tx.wait()
+  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions5, {
+    gasPrice: gasPrice,
+  });
+  receipt = await tx.wait();
   if (!receipt.status) {
-    throw Error(`Error:: ${tx.hash}`)
+    throw Error(`Error:: ${tx.hash}`);
   }
-  console.log('Uploaded item side dimensions 5')
+  console.log("Uploaded item side dimensions 5");
 
   //ID's 211 - 216
-  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions6)
-  receipt = await tx.wait()
+  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions6, {
+    gasPrice: gasPrice,
+  });
+  receipt = await tx.wait();
   if (!receipt.status) {
-    throw Error(`Error:: ${tx.hash}`)
+    throw Error(`Error:: ${tx.hash}`);
   }
-  console.log('Uploaded item side dimensions 6')
+  console.log("Uploaded item side dimensions 6");
 
   //ID's 217 - 227
-  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions7)
-  receipt = await tx.wait()
+  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions7, {
+    gasPrice: gasPrice,
+  });
+  receipt = await tx.wait();
   if (!receipt.status) {
-    throw Error(`Error:: ${tx.hash}`)
+    throw Error(`Error:: ${tx.hash}`);
   }
-  console.log('Uploaded item side dimensions 7')
+  console.log("Uploaded item side dimensions 7");
 
   //ID's 228 - 24
-  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions8)
-  receipt = await tx.wait()
+  tx = await svgViewsFacet.setSideViewDimensions(sideViewDimensions8, {
+    gasPrice: gasPrice,
+  });
+  receipt = await tx.wait();
   if (!receipt.status) {
-    throw Error(`Error:: ${tx.hash}`)
+    throw Error(`Error:: ${tx.hash}`);
   }
-  console.log('Uploaded item side dimensions 8')
+  console.log("Uploaded item side dimensions 8");
 }
 
-
-if(require.main === module){
-    main()
+if (require.main === module) {
+  main()
     .then(() => process.exit(0))
-    .catch(error => {
-      console.error(error)
-      process.exit(1)
-    })
-  }
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
 
-  exports.sideViewDimensionsUpdate = main;
+exports.sideViewDimensionsUpdate = main;
