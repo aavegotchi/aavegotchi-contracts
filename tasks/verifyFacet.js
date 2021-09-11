4c17ad9abb6c2852a2e84471488954598e7e6833
Coderdan <coderdannn@gmail.com>
2021-09-11 16:54:38 +0800

    implement typescript into generateDiamondABI

--- tasks/verifyFacet.js
+++ tasks/verifyFacet.js
@@ -4,26 +4,7 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const addresses = [
-  // '0x4f908Fa47F10bc2254dae7c74d8B797C1749A8a6', // 120 days
-  // '0x58f64b56B1e15D8C932c51287d814EDaa8d6feb9', // 120 days
-  // '0xAE7DF9f59FEc446903c64f21a76d039Bc81712ef', // 120 days
-  // '0xefD4Cc25E5D01F84411D8Fca321F74bdf65E3d02', // 88 days
-  // '0xfa7a3bb12848A7856Dd2769Cd763310096c053F1', // 6 days // AavegotchiGameFacet
-  // '0xf35c518e373D71e4966295cF1E30f0c0555Cc85F', // 44 days // SvgFacet
-  // '0xE6fC4684bb1a6A71DB11B25Be01F9D3b1eCe10c6', // 3 days
-  // '0xc317D47d094958b2D7f2e689598d3EC3fD75577F', // 88 days
-  // '0x70b03b843122887B907d177C97d0CD837cC5667c', // 44 days
-  // '0x0BfA0cfC88ff56C37e2AfA32af9BeE77f6f970ED', // 83 days // ShopFacet
-  // '0x1e09Fc5511fbFc4b4cf718b22962D1870804c279', // 31 days
-  // '0x433484AAfDa3820A851cf560F23026c375E76194', // 120 days
-  // '0x2eC212685CdEba693772dd0716551Eda4eb6965b', // 58 days
-  // '0x691a713894403bF3b8a8A871BAB0D755D4b15096', // 78 days // AavegotchiFacet
-  // '0xBffAAA85d2289ad818fd54d228cedF2efd48EF32', // 57 days // EscrowFacet
-  // '0x68B7BF18184E0cC160f046E567Cc5cdbbf0d89d6', // 55 days
-  // '0x1AbA526A0508bf55844625597F10539999caB598', // 18 days // DaoFacet
-  // '0x1e80aC550386dd9af8e308DA0144d2B17EE7fD8c' // CollateralFacet
-];
+const addresses = [];
 function getCompilerVersion(code) {
   try {
     // TODO: not sure if we should use this
