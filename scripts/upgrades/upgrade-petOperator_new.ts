da44d0bf4cae5587c802fe6aa769ae41e9f0134e
Coderdan <dmathieuva@gmail.com>
2021-09-19 21:45:52 +0800

    finalizing deployUpgrade task

---
+++ scripts/upgrades/upgrade-petOperator_new.ts
@@ -0,0 +1,47 @@
+import { run, ethers } from "hardhat";
+import {
+  convertFacetAndSelectorsToString,
+  FacetsAndAddSelectors,
+} from "../../tasks/deployUpgrade";
+import { getSelector, maticDiamondAddress } from "../helperFunctions";
+
+async function upgrade() {
+  const diamondUpgrader = "0x35fe3df776474a7b24b3b1ec6e745a830fdad351";
+  const facets: FacetsAndAddSelectors[] = [
+    {
+      facetName:
+        "contracts/Aavegotchi/facets/AavegotchiFacet.sol:AavegotchiFacet",
+      addSelectors: [
+        getSelector(
+          "function isPetOperatorForAll(address _owner, address _operator) external view returns (bool approved_)",
+          ethers
+        ),
+        getSelector(
+          "function setPetOperatorForAll(address _operator, bool _approved) external",
+          ethers
+        ),
+      ],
+    },
+  ];
+  const joined = convertFacetAndSelectorsToString(facets);
+
+  console.log("facets:", joined);
+
+  const removeSelectors: string[] = [];
+
+  await run("deployUpgrade", {
+    diamondUpgrader: diamondUpgrader,
+    diamondAddress: maticDiamondAddress,
+    facetsAndAddSelectors: joined,
+    removeSelectors: JSON.stringify(removeSelectors),
+  });
+}
+
+upgrade()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+exports.upgrade = upgrade;
--- tasks/deployUpgrade.ts
+++ tasks/deployUpgrade.ts
@@ -15,13 +15,59 @@ import { IDiamondCut } from "../typechain/IDiamondCut";
 import { getSelectors } from "../scripts/helperFunctions";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+export interface FacetsAndAddSelectors {
+  facetName: string;
+  addSelectors: string[];
+}
+
 type FacetCutType = { Add: 0; Replace: 1; Remove: 2 };
 const FacetCutAction: FacetCutType = { Add: 0, Replace: 1, Remove: 2 };
+
+interface TaskArgs {
+  diamondUpgrader: string;
+  diamondAddress: string;
+  facetsAndAddSelectors: string;
+  removeSelectors: string;
+}
 
 interface Cut {
   facetAddress: string;
   action: 0 | 1 | 2;
   functionSelectors: string[];
+}
+
+export function convertFacetAndSelectorsToString(
+  facets: FacetsAndAddSelectors[]
+): string {
+  let outputString = "";
+
+  facets.forEach((facet) => {
+    outputString = outputString.concat(
+      `#${facet.facetName}$$$${facet.addSelectors.join("*")}`
+    );
+  });
+
+  return outputString;
+}
+
+export function convertStringToFacetAndSelectors(
+  facets: string
+): FacetsAndAddSelectors[] {
+  const facetArrays: string[] = facets.split("#").filter((string) => {
+    return string.length > 0;
+  });
+
+  let output: FacetsAndAddSelectors[] = [];
+
+  facetArrays.forEach((string) => {
+    const facetsAndAddSelectors = string.split("$$$");
+    output.push({
+      facetName: facetsAndAddSelectors[0],
+      addSelectors: facetsAndAddSelectors[1].split("*"),
+    });
+  });
+
+  return output;
 }
 
 task(
@@ -30,14 +76,14 @@ task(
 )
   .addParam("diamondUpgrader", "Address of the multisig signer")
   .addParam("diamondAddress", "Address of the Diamond to upgrade")
-  .addParam("facets", "Array of facet names to upgrade")
   .addParam(
-    "addSelectors",
-    "Array of selectors arrays to add. Must be the same length as the facets array"
+    "facetsAndAddSelectors",
+    "Stringified array of facet names to upgrade, along with an array of add Selectors"
   )
-  .addOptionalParam(
+
+  .addParam(
     "removeSelectors",
-    "Array of selectors to remove from the Diamond"
+    "Stringifed array of selectors to remove from the Diamond, or empty"
   )
 
   /*Example of addSelectors array: 
@@ -45,17 +91,15 @@ task(
     getSelector('function addItemManagers(address[] calldata _newItemManagers) external'),
   ]
   */
+  .setAction(async (taskArgs: TaskArgs, hre: HardhatRuntimeEnvironment) => {
+    const facets: string = taskArgs.facetsAndAddSelectors;
+    const facetsAndAddSelectors: FacetsAndAddSelectors[] =
+      convertStringToFacetAndSelectors(facets);
 
-  .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
-    const facets: string[] = taskArgs.facets;
     const diamondUpgrader: string = taskArgs.diamondUpgrader;
-    const newSelectorsArray: string[][] = taskArgs.addSelectors;
-    const removeSelectors: string[] = taskArgs.removeSelectors;
-    const diamondAddress: string = taskArgs.diamondAddress;
 
-    if (facets.length !== newSelectorsArray.length) {
-      throw "Facets and selectors array must be the same length";
-    }
+    const removeSelectors: string = taskArgs.removeSelectors;
+    const diamondAddress: string = taskArgs.diamondAddress;
 
     //Instantiate the Signer
     let signer: Signer;
@@ -83,9 +127,12 @@ task(
     let deployedFacets = [];
     let cut: Cut[] = [];
 
-    facets.forEach(async (facet: string, index: number) => {
+    for (let index = 0; index < facetsAndAddSelectors.length; index++) {
+      const facet = facetsAndAddSelectors[index];
+
+      console.log("facet:", facet);
       let factory = (await hre.ethers.getContractFactory(
-        facet
+        facet.facetName
       )) as ContractFactory;
       let deployedFacet: Contract = await factory.deploy();
       await deployedFacet.deployed();
@@ -95,12 +142,16 @@ task(
       );
       deployedFacets.push(deployedFacet);
 
-      const newSelectors = newSelectorsArray[index];
+      const newSelectors = facet.addSelectors;
+
+      console.log("new selectors:", newSelectors);
 
       let existingSelectors = getSelectors(deployedFacet);
       existingSelectors = existingSelectors.filter(
         (selector) => !newSelectors.includes(selector)
       );
+
+      console.log("existing selectors:", existingSelectors);
 
       if (newSelectors.length > 0) {
         cut.push({
@@ -118,14 +169,14 @@ task(
       });
 
       //todo: add Remove facets
-    });
+    }
 
-    if (removeSelectors.length > 0) {
+    if (JSON.parse(removeSelectors).length > 0) {
       console.log("Removing selectors:", removeSelectors);
       cut.push({
         facetAddress: hre.ethers.constants.AddressZero,
         action: FacetCutAction.Remove,
-        functionSelectors: removeSelectors,
+        functionSelectors: JSON.parse(removeSelectors),
       });
     }
 
