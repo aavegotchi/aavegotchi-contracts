833c2191acaf35a72069f49d0cdd9feb1e356156
Coderdan <dmathieuva@gmail.com>
2021-09-25 11:55:02 +0800

    added some types

--- scripts/updates/addR5sideViews.ts
+++ scripts/updates/addR5sideViews.ts
@@ -1,4 +1,4 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 
 import {
   wearablesLeftSvgs,
@@ -9,15 +9,11 @@ import {
   wearablesBackSleeveSvgs,
 } from "../../svgs/wearables-sides.js";
 
-import {
-  sideViewDimensions9
-} from "../../svgs/sideViewDimensions.js";
+import { sideViewDimensions9 } from "../../svgs/sideViewDimensions.js";
 
 /* const hre = require("hardhat"); */
 
-
-async function main(){
-
+async function main() {
   const gasPrice = 100000000000;
   const diamondAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
   let account1Signer;
@@ -27,10 +23,10 @@ async function main(){
   let owner = await (
     await ethers.getContractAt("OwnershipFacet", diamondAddress)
   ).owner();
-  const testing = ["hardhat", "localhost"].includes(hre.network.name);
+  const testing = ["hardhat", "localhost"].includes(network.name);
 
   if (testing) {
-    await hre.network.provider.request({
+    await network.provider.request({
       method: "hardhat_impersonateAccount",
       params: [owner],
     });
@@ -44,7 +40,7 @@ async function main(){
       throw Error(`Error:: ${tx.hash}`);
     }
     /* console.log("assigned", account1Address, "as item manager"); */
-  } else if (hre.network.name === "matic") {
+  } else if (network.name === "matic") {
     const accounts = await ethers.getSigners();
     const account = await accounts[0].getAddress();
     /* console.log("account:", account); */
@@ -54,7 +50,13 @@ async function main(){
     throw Error("Incorrect network selected");
   }
 
-  async function updateSvgs(svg: any, svgType: string, svgId: number, testing: boolean, uploadSigner: any) {
+  async function updateSvgs(
+    svg: any,
+    svgType: string,
+    svgId: number,
+    testing: boolean,
+    uploadSigner: any
+  ) {
     const svgFacet = await ethers.getContractAt(
       "SvgFacet",
       diamondAddress,
@@ -124,20 +126,38 @@ async function main(){
   }
 
   //sleeves
-  const updatingSleevesLeft = [36, 37, 38, 39, 40]
-  const updatingSleevesRight = [36, 37, 38, 39, 40]
-  const updatingSleevesBack = [36, 37, 38, 39, 40]
+  const updatingSleevesLeft = [36, 37, 38, 39, 40];
+  const updatingSleevesRight = [36, 37, 38, 39, 40];
+  const updatingSleevesBack = [36, 37, 38, 39, 40];
 
   for (var i = 0; i < updatingSleevesLeft.length; i++) {
-    await updateSvgs(wearablesLeftSleeveSvgs, 'sleeves-left', updatingSleevesLeft[i], testing, itemSigner)
+    await updateSvgs(
+      wearablesLeftSleeveSvgs,
+      "sleeves-left",
+      updatingSleevesLeft[i],
+      testing,
+      itemSigner
+    );
   }
 
   for (var i = 0; i < updatingSleevesRight.length; i++) {
-    await updateSvgs(wearablesRightSleeveSvgs, 'sleeves-right', updatingSleevesRight[i], testing, itemSigner)
+    await updateSvgs(
+      wearablesRightSleeveSvgs,
+      "sleeves-right",
+      updatingSleevesRight[i],
+      testing,
+      itemSigner
+    );
   }
 
   for (var i = 0; i < updatingSleevesBack.length; i++) {
-    await updateSvgs(wearablesBackSleeveSvgs, 'sleeves-back', updatingSleevesBack[i], testing, itemSigner)
+    await updateSvgs(
+      wearablesBackSleeveSvgs,
+      "sleeves-back",
+      updatingSleevesBack[i],
+      testing,
+      itemSigner
+    );
   }
 
   //dimensions
@@ -155,7 +175,7 @@ async function main(){
     throw Error(`Error:: ${tx.hash}`);
   }
 
-// **** Test ****
+  // **** Test ****
   // BODY = 0;
   // FACE = 1;
   // EYES = 2;
@@ -168,9 +188,13 @@ async function main(){
   const numTraits1 = [99, 99, 99, 99, 12, 9];
   const wearables1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-  const sidePreview = await svgViewsFacet.previewSideAavegotchi("2", "0xE0b22E0037B130A9F56bBb537684E6fA18192341", numTraits1, wearables1);
+  const sidePreview = await svgViewsFacet.previewSideAavegotchi(
+    "2",
+    "0xE0b22E0037B130A9F56bBb537684E6fA18192341",
+    numTraits1,
+    wearables1
+  );
   console.log("Side Preview: ", sidePreview);
-
 }
 
 main()
@@ -180,4 +204,4 @@ main()
     process.exit(1);
   });
 
-  exports.addR5sideViews = main;
+exports.addR5sideViews = main;
