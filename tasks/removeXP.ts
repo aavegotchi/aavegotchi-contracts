b7ebf3a4ad61641ad1b9ff22de82c0b908bd3998
Coderdan <dmathieuva@gmail.com>
2021-09-30 14:05:21 +0800

    finalize removeXP task

--- tasks/removeXP.ts
+++ tasks/removeXP.ts
@@ -23,26 +23,6 @@ interface Data {
   gotchisOwned: GotchisOwned[];
 }
 
-interface AddressCounts {
-  [key: string]: number;
-}
-
-function strDisplay(str: string) {
-  return addCommas(str.toString());
-}
-
-function addCommas(nStr: string) {
-  nStr += "";
-  const x = nStr.split(".");
-  let x1 = x[0];
-  const x2 = x.length > 1 ? "." + x[1] : "";
-  var rgx = /(\d+)(\d{3})/;
-  while (rgx.test(x1)) {
-    x1 = x1.replace(rgx, "$1" + "," + "$2");
-  }
-  return x1 + x2;
-}
-
 task("removeXP", "Removes XP from Gotchis")
   .addParam("filename", "File that contains the airdrop")
 
