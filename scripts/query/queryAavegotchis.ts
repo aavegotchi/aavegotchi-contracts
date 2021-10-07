a39deef950743e851b578d8ca9e4fd7d2225eba9
Temitayo Daniel <akinnusotutemitayodaniel@gmail.com>
2021-10-07 04:56:55 +0100

    in progress

---
+++ scripts/query/queryAavegotchis.ts
@@ -0,0 +1,34 @@
+const { request, gql } = require("graphql-request");
+const {
+  LiquidityManagerSigProp,
+} = require("../../data/airdrops/sigprop/LiquidityManagerSigProp.ts");
+let queryData: any;
+
+async function out() {
+  queryData = `
+  {users(where:{id_in:[${LiquidityManagerSigProp.map(
+    (add: string) => '"' + add + '"'
+  )}]},first:1000) {
+    id
+    gotchisOwned(first:1000) {
+      id
+    }}
+    
+    users(where:{id_in:[${LiquidityManagerSigProp.map(
+      (add: string) => '"' + add + '"'
+    )}]},first:100,skip:100) {
+      id
+      gotchisOwned(first:1000) {
+        id
+      }
+  }
+  }
+`;
+  request(
+    "https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-core-matic",
+    queryData
+  ).then((data: { users: any }) =>
+    console.log(JSON.stringify(data.users), data.users.length)
+  );
+}
+out();
