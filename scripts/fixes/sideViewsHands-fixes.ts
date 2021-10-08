6784e265371b7b5e3e42806c57f599bb92705d1e
Coderdan <dmathieuva@gmail.com>
2021-10-08 13:00:02 +0800

    use new dimensions

--- scripts/fixes/sideViewsHands-fixes.ts
+++ scripts/fixes/sideViewsHands-fixes.ts
@@ -84,11 +84,7 @@ async function main() {
 
   await run(
     "updateItemSideDimensions",
-    convertSideDimensionsToTaskFormat(sideViewDimensions1)
-  );
-  await run(
-    "updateItemSideDimensions",
-    convertSideDimensionsToTaskFormat(sideViewDimensions8)
+    convertSideDimensionsToTaskFormat(newDimensions)
   );
 }
 
