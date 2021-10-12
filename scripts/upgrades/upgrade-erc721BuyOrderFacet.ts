adb5f4a8a8a71ade60dfb41b98aaa97edd30863a
orionstardust <justinono1990@outlook.com>
2021-10-11 22:16:11 -0700

    Add deploy script for ERC721BuyOrderFacet

---
+++ scripts/upgrades/upgrade-erc721BuyOrderFacet.ts
@@ -0,0 +1,47 @@
+import { run } from "hardhat";
+import {
+  convertFacetAndSelectorsToString,
+  DeployUpgradeTaskArgs,
+  FacetsAndAddSelectors,
+} from "../../tasks/deployUpgrade";
+import { maticDiamondAddress } from "../helperFunctions";
+
+export async function upgrade() {
+  const diamondUpgrader = "0x35fe3df776474a7b24b3b1ec6e745a830fdad351";
+
+  const facets: FacetsAndAddSelectors[] = [
+    {
+      facetName: "ERC721BuyOrderFacet",
+      addSelectors: [
+        "function getERC721BuyOrder(uint256 _buyOrderId) external view",
+        "function getERC721BuyOrderByTokenId(uint256 _erc721TokenId) external view",
+        "function placeERC721BuyOrder(address _erc721TokenAddress, uint256 _erc721TokenId, uint256 _priceInWei) external",
+        "function cancelERC721BuyOrderByToken(uint256 _erc721TokenId) external",
+        "function cancelERC721BuyOrder(uint256 _buyOrderId) external",
+      ],
+      removeSelectors: [],
+    },
+  ];
+
+  const joined = convertFacetAndSelectorsToString(facets);
+
+  const args: DeployUpgradeTaskArgs = {
+    diamondUpgrader: diamondUpgrader,
+    diamondAddress: maticDiamondAddress,
+    facetsAndAddSelectors: joined,
+    useLedger: true,
+    useMultisig: true,
+  };
+
+  await run("deployUpgrade", args);
+}
+
+if (require.main === module) {
+  upgrade()
+    .then(() => process.exit(0))
+    // .then(() => console.log('upgrade completed') /* process.exit(0) */)
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
