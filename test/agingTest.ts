881023392d6aa0dc2c36e75aa036a1cf16e19868
marcoruggeri <4high20@gmail.com>
2021-10-12 16:37:55 +0100

    update tests

--- test/agingTest.ts
+++ test/agingTest.ts
@@ -4,43 +4,180 @@
 //@ts-ignore
 import { ethers } from "hardhat";
 import { expect } from "chai";
-import { AavegotchiGameFacet } from "../typechain";
+import { AavegotchiGameFacet, AavegotchiFacet } from "../typechain";
 import { upgrade } from "../scripts/upgrades/upgrade-aging";
+import { BigNumber } from "@ethersproject/bignumber";
 
 describe("Testing Aging", async function () {
   const diamondAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
   let aavegotchiGameFacet: AavegotchiGameFacet;
-  before(async function () {
-    await upgrade();
+  let aavegotchiFacet: AavegotchiFacet;
+  interface Data {
+    gotchiId: number;
+    skillPointsBefore: BigNumber | null;
+    skillPointsAfter: BigNumber | null;
+    claimTime: BigNumber | null;
+    age: number;
+    ageSkillPointsTest: number;
+    testSkillPoints: number;
+    contractSkillPoints: BigNumber | null;
+  }
 
+  let data: Data[] = [
+    {
+      gotchiId: 6908,
+      skillPointsBefore: null,
+      skillPointsAfter: null,
+      claimTime: null,
+      age: 0,
+      ageSkillPointsTest: 0,
+      testSkillPoints: 0,
+      contractSkillPoints: null,
+    },
+    {
+      gotchiId: 15560,
+      skillPointsBefore: null,
+      skillPointsAfter: null,
+      claimTime: null,
+      age: 0,
+      ageSkillPointsTest: 0,
+      testSkillPoints: 0,
+      contractSkillPoints: null,
+    },
+    {
+      gotchiId: 22324,
+      skillPointsBefore: null,
+      skillPointsAfter: null,
+      claimTime: null,
+      age: 0,
+      ageSkillPointsTest: 0,
+      testSkillPoints: 0,
+      contractSkillPoints: null,
+    },
+    {
+      gotchiId: 16559,
+      skillPointsBefore: null,
+      skillPointsAfter: null,
+      claimTime: null,
+      age: 0,
+      ageSkillPointsTest: 0,
+      testSkillPoints: 0,
+      contractSkillPoints: null,
+    },
+    {
+      gotchiId: 5205,
+      skillPointsBefore: null,
+      skillPointsAfter: null,
+      claimTime: null,
+      age: 0,
+      ageSkillPointsTest: 0,
+      testSkillPoints: 0,
+      contractSkillPoints: null,
+    },
+    {
+      gotchiId: 9369,
+      skillPointsBefore: null,
+      skillPointsAfter: null,
+      claimTime: null,
+      age: 0,
+      ageSkillPointsTest: 0,
+      testSkillPoints: 0,
+      contractSkillPoints: null,
+    },
+    {
+      gotchiId: 22197,
+      skillPointsBefore: null,
+      skillPointsAfter: null,
+      claimTime: null,
+      age: 0,
+      ageSkillPointsTest: 0,
+      testSkillPoints: 0,
+      contractSkillPoints: null,
+    },
+    {
+      gotchiId: 13996,
+      skillPointsBefore: null,
+      skillPointsAfter: null,
+      claimTime: null,
+      age: 0,
+      ageSkillPointsTest: 0,
+      testSkillPoints: 0,
+      contractSkillPoints: null,
+    },
+    {
+      gotchiId: 11663,
+      skillPointsBefore: null,
+      skillPointsAfter: null,
+      claimTime: null,
+      age: 0,
+      ageSkillPointsTest: 0,
+      testSkillPoints: 0,
+      contractSkillPoints: null,
+    },
+    {
+      gotchiId: 15243,
+      skillPointsBefore: null,
+      skillPointsAfter: null,
+      claimTime: null,
+      age: 0,
+      ageSkillPointsTest: 0,
+      testSkillPoints: 0,
+      contractSkillPoints: null,
+    },
+  ];
+  before(async function () {
     aavegotchiGameFacet = (await ethers.getContractAt(
       "AavegotchiGameFacet",
       diamondAddress
     )) as AavegotchiGameFacet;
+    aavegotchiFacet = (await ethers.getContractAt(
+      "contracts/Aavegotchi/facets/AavegotchiFacet.sol:AavegotchiFacet",
+      diamondAddress
+    )) as AavegotchiFacet;
+    for (let i = 0; i < data.length; i++) {
+      data[i].skillPointsBefore =
+        await aavegotchiGameFacet.availableSkillPoints(data[i].gotchiId);
+    }
+
+    await upgrade();
   });
 
-  it("Check against top 10 BRS", async function () {
-    const g1 = await aavegotchiGameFacet.availableSkillPoints(6908);
-    expect(g1).to.equal(6);
-    const g2 = await aavegotchiGameFacet.availableSkillPoints(15560);
-    expect(g2).to.equal(1);
-    const g3 = await aavegotchiGameFacet.availableSkillPoints(22324);
-    expect(g3).to.equal(1);
-    const g4 = await aavegotchiGameFacet.availableSkillPoints(16559);
-    expect(g4).to.equal(1);
-    const g5 = await aavegotchiGameFacet.availableSkillPoints(5205);
-    expect(g5).to.equal(5);
-    const g6 = await aavegotchiGameFacet.availableSkillPoints(9369);
-    expect(g6).to.equal(5);
-    const g7 = await aavegotchiGameFacet.availableSkillPoints(22197);
-    expect(g7).to.equal(1);
-    const g8 = await aavegotchiGameFacet.availableSkillPoints(13996);
-    expect(g8).to.equal(1);
-    const g9 = await aavegotchiGameFacet.availableSkillPoints(11663);
-    expect(g9).to.equal(1);
-    const g10 = await aavegotchiGameFacet.availableSkillPoints(15243);
-    expect(g10).to.equal(0);
-    const g11 = await aavegotchiGameFacet.availableSkillPoints(2057);
-    expect(g11).to.equal(4);
+  it("Compare skill points before and after upgrade", async function () {
+    for (let i = 0; i < data.length; i++) {
+      data[i].skillPointsAfter = await aavegotchiGameFacet.availableSkillPoints(
+        data[i].gotchiId
+      );
+      data[i].claimTime = await aavegotchiFacet.aavegotchiClaimTime(
+        data[i].gotchiId
+      );
+      data[i].age = Math.floor(Date.now() / 1000) - Number(data[i].claimTime);
+      // check only if gotchi is atleast 1M blocks old
+      if (data[i].age > 2300000) {
+        expect(data[i].skillPointsAfter).to.above(data[i].skillPointsBefore);
+      }
+    }
+  });
+
+  it("Check that skill points matches", async function () {
+    let fibSequence = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+    const calcSkillPoints = (age: number) => {
+      let skillPoints = 0;
+      for (let i = 0; i < fibSequence.length; i++) {
+        if (age > fibSequence[i] * 2300000) {
+          skillPoints++;
+        }
+      }
+      return skillPoints;
+    };
+    for (let i = 0; i < data.length; i++) {
+      data[i].ageSkillPointsTest = calcSkillPoints(data[i].age);
+      data[i].testSkillPoints =
+        data[i].ageSkillPointsTest + Number(data[i].skillPointsBefore);
+      data[i].contractSkillPoints =
+        await aavegotchiGameFacet.availableSkillPoints(data[i].gotchiId);
+      expect(data[i].testSkillPoints).to.equal(
+        Number(data[i].contractSkillPoints)
+      );
+    }
   });
 });
