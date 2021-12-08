323e51188ebaf699418727658753c41564c90532
Mark Watson <markjwatson86@gmail.com>
2021-12-07 20:36:44 -0800

    added tasks for adding baadge svg and baadge airdrop

--- tasks/addBaadgeSvgs.ts
+++ tasks/addBaadgeSvgs.ts
@@ -1,27 +1,13 @@
 import { LedgerSigner } from "@ethersproject/hardware-wallets";
 
 import { task } from "hardhat/config";
-import {
-  Contract,
-  ContractReceipt,
-  ContractTransaction,
-} from "@ethersproject/contracts";
+import { ContractReceipt, ContractTransaction } from "@ethersproject/contracts";
 import { Signer } from "@ethersproject/abstract-signer";
-import {
-  getItemTypes,
-  ItemTypeOutput,
-  SleeveObject,
-} from "../scripts/itemTypeHelpers";
+import { getItemTypes, ItemTypeOutput } from "../scripts/itemTypeHelpers";
 import { DAOFacet } from "../typechain/DAOFacet";
 import { SvgFacet } from "../typechain/SvgFacet";
 import { BigNumberish } from "@ethersproject/bignumber";
-import {
-  setupSvg,
-  printSizeInfo,
-  svgTypeToBytes,
-  uploadSvgs,
-  updateSvgs,
-} from "../scripts/svgHelperFunctions";
+import { uploadOrUpdateSvg } from "../scripts/svgHelperFunctions";
 import { gasPrice } from "../scripts/helperFunctions";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
@@ -30,14 +16,9 @@ export interface AddItemTypesTaskArgs {
   diamondAddress: string;
   itemFile: string;
   svgFile: string;
-  sleeveStartId: string;
+  svgIds: string;
   uploadItemTypes: boolean;
-  uploadWearableSvgs: boolean;
-  associateSleeves: boolean;
   sendToItemManager: boolean;
-  replaceWearableSvgs: boolean;
-  replaceSleeveSvgs: boolean;
-  uploadSleeveSvgs: boolean;
 }
 
 task("addItemTypes", "Adds itemTypes and SVGs ")
@@ -45,41 +26,32 @@ task("addItemTypes", "Adds itemTypes and SVGs ")
   .addParam("diamondAddress", "Address of the Diamond to upgrade")
   .addParam("itemFile", "File name of the items to add")
   .addParam("svgFile", "File name of the itemType SVGs")
-  .addParam("sleeveStartId", "ID of the sleeve to start at")
+  .addParam("svgIds", "List of SVG IDs to add or update")
   .addFlag("uploadItemTypes", "Upload itemTypes")
-  .addFlag("uploadWearableSvgs", "Upload the SVGs")
-  .addFlag("uploadSleeveSvgs", "Upload sleeve svgs")
-  .addFlag("associateSleeves", "Associate the sleeves")
   .addFlag("sendToItemManager", "Mint and send the items to itemManager")
-  .addFlag("replaceWearableSvgs", "Replace wearable SVGs instead of uploading")
-  .addFlag("replaceSleeveSvgs", "Replace sleeve SVGs instead of uploading")
 
   .setAction(
     async (taskArgs: AddItemTypesTaskArgs, hre: HardhatRuntimeEnvironment) => {
       const itemFile: string = taskArgs.itemFile;
       const diamondAddress: string = taskArgs.diamondAddress;
       const svgFile: string = taskArgs.svgFile;
-      const sleeveStartId: string = taskArgs.sleeveStartId;
       const itemManager = taskArgs.itemManager;
+      const svgIds: string[] = taskArgs.svgIds
+        .split(",")
+        .filter((str) => str.length > 0);
       const sendToItemManager = taskArgs.sendToItemManager;
       const uploadItemTypes = taskArgs.uploadItemTypes;
-      const uploadWearableSvgs = taskArgs.uploadWearableSvgs;
-      const associateSleeves = taskArgs.associateSleeves;
-      const uploadSleeveSvgs = taskArgs.uploadSleeveSvgs;
-      const replaceWearableSvgs = taskArgs.replaceWearableSvgs;
-      const replaceSleeveSvgs = taskArgs.replaceSleeveSvgs;
 
       const {
         itemTypes: currentItemTypes,
       } = require(`../data/itemTypes/${itemFile}.ts`);
 
       const { getWearables } = require(`../svgs/${svgFile}.ts`);
-      const { sleeves, wearables } = getWearables();
+      const { wearables } = getWearables();
 
       const itemTypesArray: ItemTypeOutput[] = getItemTypes(currentItemTypes);
 
       const svgsArray: string[] = wearables;
-      const sleeveSvgsArray: SleeveObject[] = sleeves;
 
       let signer: Signer;
 
@@ -116,6 +88,22 @@ task("addItemTypes", "Adds itemTypes and SVGs ")
         signer
       )) as SvgFacet;
 
+      for (let index = 0; index < svgIds.length; index++) {
+        const svgId = svgIds[index];
+
+        try {
+          await uploadOrUpdateSvg(
+            svgsArray[index],
+            "wearables",
+            Number(svgId),
+            svgFacet,
+            hre.ethers
+          );
+        } catch (error) {
+          console.log("error uploading", svgId, error);
+        }
+      }
+
       if (uploadItemTypes) {
         console.log("Adding items", 0, "to", currentItemTypes.length);
 
@@ -128,69 +116,6 @@ task("addItemTypes", "Adds itemTypes and SVGs ")
           throw Error(`Error:: ${tx.hash}`);
         }
         console.log("Items were added:", tx.hash);
-      }
-
-      if (uploadWearableSvgs) {
-        console.log("svgs array:", svgsArray);
-
-        console.log("Upload SVGs");
-        await uploadSvgs(svgFacet, svgsArray, "wearables", hre.ethers);
-      }
-
-      if (replaceWearableSvgs) {
-        await updateSvgs(
-          svgsArray,
-          "wearables",
-          itemTypesArray.map((value) => Number(value.svgId)),
-          svgFacet,
-          hre
-        );
-      }
-
-      if (uploadSleeveSvgs) {
-        console.log("Uploading Sleeves");
-
-        await uploadSvgs(
-          svgFacet,
-          sleeveSvgsArray.map((value) => value.svg),
-          "sleeves",
-          hre.ethers
-        );
-      }
-
-      if (replaceSleeveSvgs) {
-        await updateSvgs(
-          sleeveSvgsArray.map((value) => value.svg),
-          "sleeves",
-          sleeveSvgsArray.map((_, index) => Number(sleeveStartId) + index),
-          svgFacet,
-          hre
-        );
-      }
-
-      if (associateSleeves) {
-        interface SleeveInput {
-          sleeveId: BigNumberish;
-          wearableId: BigNumberish;
-        }
-
-        let sleevesSvgId: number = Number(sleeveStartId);
-        let sleevesInput: SleeveInput[] = [];
-        for (const sleeve of sleeveSvgsArray) {
-          sleevesInput.push({
-            sleeveId: sleevesSvgId,
-            wearableId: sleeve.id,
-          });
-          sleevesSvgId++;
-        }
-
-        console.log("Associating sleeves svgs with body wearable svgs.");
-        tx = await svgFacet.setSleeves(sleevesInput, { gasPrice: gasPrice });
-        receipt = await tx.wait();
-        if (!receipt.status) {
-          throw Error(`Error:: ${tx.hash}`);
-        }
-        console.log("Sleeves associated:", tx.hash);
       }
 
       if (sendToItemManager) {
---
+++ tasks/baadgeAirdrop.ts
@@ -0,0 +1,117 @@
+import { task } from "hardhat/config";
+import { Signer } from "@ethersproject/abstract-signer";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import {
+  maticDiamondAddress,
+  gameManager,
+  getDiamondSigner,
+} from "../scripts/helperFunctions";
+
+export interface AirdropBaadgeTaskArgs {
+  maxProcess: string;
+  badgeIds: string;
+  awardsArray: string;
+}
+
+task(
+  "airdropBaadges",
+  "Airdrop baadges given badgeIds to awardsArray within MaxProcess"
+)
+  .addParam("maxProcess", "Maxium amount of baadges to be airdropped")
+  .addParam("badgeIds", "List of SVG IDs to be airdropped")
+  .addParam("awardsArray", "Gotchi Id recipients of baadge airdrop")
+
+  .setAction(
+    async (taskArgs: AirdropBaadgeTaskArgs, hre: HardhatRuntimeEnvironment) => {
+      const badgeIds: string[] = taskArgs.badgeIds
+        .split(",")
+        .filter((str) => str.length > 0);
+      const maxProcess: string = taskArgs.maxProcess;
+      const awardsArray: string[] = taskArgs.awardsArray
+        .split(",")
+        .filter((str) => str.length > 0);
+
+      const finalRewards: any = {};
+      let tokenIds: number[] = [];
+      let _ids: any[] = [];
+      let _values: any[] = [];
+
+      awardsArray.forEach((winnersArray, index) => {
+        console.log("winners array:", winnersArray);
+        let badgeId = badgeIds[index];
+        for (let index = 0; index < winnersArray.length; index++) {
+          finalRewards[winnersArray[index]] = [badgeId];
+        }
+      });
+
+      Object.keys(finalRewards).forEach((key) => {
+        let values = finalRewards[key];
+        tokenIds.push(Number(key));
+        _ids.push(Number(values));
+        _values.push(Array(values.length).fill(1));
+      });
+
+      const txData = [];
+
+      let txGroup = [];
+      let tokenIdsNum = 0;
+
+      for (let index = 0; index < tokenIds.length; index++) {
+        if (Number(maxProcess) < tokenIdsNum + 1) {
+          txData.push(txGroup);
+          txGroup = [];
+          tokenIdsNum = 0;
+        }
+
+        txGroup.push({
+          index,
+        });
+        tokenIdsNum += 1;
+      }
+
+      if (tokenIdsNum > 0) {
+        txData.push(txGroup);
+        txGroup = [];
+        tokenIdsNum = 0;
+      }
+
+      const signer: Signer = await getDiamondSigner(hre, gameManager, false);
+
+      const itemsTransferFacet = await hre.ethers.getContractAt(
+        "ItemsTransferFacet",
+        maticDiamondAddress,
+        signer
+      );
+      const itemsFacet = await hre.ethers.getContractAt(
+        "contracts/Aavegotchi/facets/ItemsFacet.sol:ItemsFacet",
+        maticDiamondAddress,
+        signer
+      );
+
+      for (const [i, txGroup] of txData.entries()) {
+        let batchBeginning = txGroup[0].index;
+        let batchEnd = txGroup[txGroup.length - 1].index;
+        let batchTokenIds = tokenIds.slice(batchBeginning, batchEnd + 1);
+        let batchBadgeIds = _ids.slice(batchBeginning, batchEnd + 1);
+        let batchBadgeValues = _values.slice(batchBeginning, batchEnd + 1);
+
+        console.log(
+          `Sending Batch ${i} to tokenIDs ${batchBeginning}-${batchEnd}`
+        );
+
+        const tx = await itemsTransferFacet.batchBatchTransferToParent(
+          gameManager,
+          maticDiamondAddress,
+          batchTokenIds,
+          batchBadgeIds,
+          batchBadgeValues
+        );
+        console.log("Tx hash:", tx.hash);
+        let receipt = await tx.wait();
+
+        if (!receipt.status) {
+          throw Error(`Error:: ${tx.hash}`);
+        }
+      }
+    }
+  );
