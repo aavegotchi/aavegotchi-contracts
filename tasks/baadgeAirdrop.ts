7e7c253abbf4e659d072a67044896f3674e25e19
Mark Watson <markjwatson86@gmail.com>
2021-12-09 17:56:50 -0800

    added while loop for maxProcess and updates batch arrays

--- tasks/baadgeAirdrop.ts
+++ tasks/baadgeAirdrop.ts
@@ -31,51 +31,25 @@ task(
         .split(",")
         .filter((str) => str.length > 0);
 
-      const finalRewards: any = {};
       let tokenIds: number[] = [];
-      let _ids: any[] = [];
-      let _values: any[] = [];
+      let _ids: Number[] = [];
+      let _values: Number[] = [];
+      let batchIds: any[] = [];
+      let batchValues: any[] = [];
 
-      awardsArray.forEach((winnersArray, index) => {
-        console.log("winners array:", winnersArray);
-        let badgeId = badgeIds[index];
-        for (let index = 0; index < winnersArray.length; index++) {
-          /* finalRewards[winnersArray[index]] = [badgeId]; */
-          finalRewards.recipients = winnersArray[index];
-          finalRewards.badgeId = [badgeId];
-          finalRewards.value = 1;
-        }
-      });
-
-      Object.keys(finalRewards).forEach((key) => {
-        let values = finalRewards[key];
-        tokenIds.push(Number(key));
-        _ids.push(Number(values));
-        _values.push(Array(values.length).fill(1));
-      });
-
-      const txData = [];
-
-      let txGroup = [];
       let tokenIdsNum = 0;
 
-      for (let index = 0; index < tokenIds.length; index++) {
-        if (Number(maxProcess) < tokenIdsNum + 1) {
-          txData.push(txGroup);
-          txGroup = [];
-          tokenIdsNum = 0;
+      while (Number(maxProcess) < tokenIdsNum + 1) {
+        for (let index = 0; index < awardsArray.length; index++) {
+          for (let index = 0; index < badgeIds.length; index++) {
+            _ids.push(Number(badgeIds[index]));
+            _values.push(1);
+          }
+          tokenIds.push(Number(awardsArray[index]));
+          batchIds.push(_ids);
+          batchValues.push(_values);
         }
-
-        txGroup.push({
-          index,
-        });
         tokenIdsNum += 1;
-      }
-
-      if (tokenIdsNum > 0) {
-        txData.push(txGroup);
-        txGroup = [];
-        tokenIdsNum = 0;
       }
 
       const signer: Signer = await getDiamondSigner(hre, gameManager, false);
@@ -85,36 +59,19 @@ task(
         maticDiamondAddress,
         signer
       );
-      const itemsFacet = await hre.ethers.getContractAt(
-        "contracts/Aavegotchi/facets/ItemsFacet.sol:ItemsFacet",
+
+      const tx = await itemsTransferFacet.batchBatchTransferToParent(
+        gameManager,
         maticDiamondAddress,
-        signer
+        tokenIds,
+        batchIds,
+        batchValues
       );
+      console.log("Tx hash:", tx.hash);
+      let receipt = await tx.wait();
 
-      for (const [i, txGroup] of txData.entries()) {
-        let batchBeginning = txGroup[0].index;
-        let batchEnd = txGroup[txGroup.length - 1].index;
-        let batchTokenIds = tokenIds.slice(batchBeginning, batchEnd + 1);
-        let batchBadgeIds = _ids.slice(batchBeginning, batchEnd + 1);
-        let batchBadgeValues = _values.slice(batchBeginning, batchEnd + 1);
-
-        console.log(
-          `Sending Batch ${i} to tokenIDs ${batchBeginning}-${batchEnd}`
-        );
-
-        const tx = await itemsTransferFacet.batchBatchTransferToParent(
-          gameManager,
-          maticDiamondAddress,
-          batchTokenIds,
-          batchBadgeIds,
-          batchBadgeValues
-        );
-        console.log("Tx hash:", tx.hash);
-        let receipt = await tx.wait();
-
-        if (!receipt.status) {
-          throw Error(`Error:: ${tx.hash}`);
-        }
+      if (!receipt.status) {
+        throw Error(`Error:: ${tx.hash}`);
       }
     }
   );
