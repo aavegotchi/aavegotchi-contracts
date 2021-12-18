58776353ad286ce7c7a9e74cfd49cd1f1f07a51d
Mark Watson <markjwatson86@gmail.com>
2021-12-17 18:11:08 -0800

    test update

--- test/sn2BaadgeAirdropTest.ts
+++ test/sn2BaadgeAirdropTest.ts
@@ -34,7 +34,7 @@ describe("Airdrop SZN2 Baadges", async function () {
     signer = await ethers.getSigner(maticDiamondAddress);
 
     aavegotchiFacet = (await ethers.getContractAt(
-      "AavegotchiFacet",
+      "contracts/Aavegotchi/facets/AavegotchiFacet.sol:AavegotchiFacet",
       maticDiamondAddress,
       signer
     )) as AavegotchiFacet;
@@ -43,7 +43,22 @@ describe("Airdrop SZN2 Baadges", async function () {
   });
 
   it.only("Should airdrop szn2 baadges", async function () {
-    let rookKinTopTen = await aavegotchiFacet.getAavegotchi(topTenRookKin[0]);
-    console.log("topTenRookKin[0]: ", rookKinTopTen);
+    // for (let index = 0; index < topTenRookKin.length; index++) {
+    //   let rookKinTopTen = await aavegotchiFacet.getAavegotchi(
+    //     topTenRookKin[index]
+    //   );
+    //   let items = rookKinTopTen.items;
+    //   for (let index = 0; index < items.length; index++) {
+    //     let svgId = items[index];
+    //     svgId[svgId.length - 1];
+    //   }
+
+    // }
+
+    let rookKinTopTen = await aavegotchiFacet.getAavegotchi(rookKin[10]);
+    let item = rookKinTopTen.items[rookKinTopTen.items.length - 1];
+    let baadge = item[item.length - 1];
+
+    console.log("topTenRookKin: ", baadge);
   });
 });
