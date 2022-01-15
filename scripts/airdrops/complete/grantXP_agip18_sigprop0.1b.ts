208b0a97c1b1cf9ddfd554acb35a27f645bc620f
Coderdan <dmathieuva@gmail.com>
2022-01-15 20:58:02 +0800

    Update grantXP_agip18_sigprop0.1b.ts
    fix proposalId

--- scripts/airdrops/complete/grantXP_agip18_sigprop0.1b.ts
+++ scripts/airdrops/complete/grantXP_agip18_sigprop0.1b.ts
@@ -4,7 +4,7 @@ import { GrantXPSnapshotTaskArgs } from "../../../tasks/grantXP_snapshot";
 async function grantXP() {
   const taskArgs: GrantXPSnapshotTaskArgs = {
     proposalId:
-      "0x76c52e544a20c03d0e9c074a86b5832060fdce7ea6bf781f035dd37efbb82f4f0",
+      "0x76c52e544a20c03d0e9c074a86b5832060fdce7ea6bf781f035dd37efbb82f4f",
     propType: "sigprop",
     batchSize: "500",
   };
