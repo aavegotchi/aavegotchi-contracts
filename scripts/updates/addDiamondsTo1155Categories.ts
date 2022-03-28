601d02b87660c29fbadee4264c62ea1c0107a220
orionstardust <justinono1990@outlook.com>
2022-03-28 14:33:20 -0700

    Add update script for adding ERC1155 categories for both installation and tile diamonds

---
+++ scripts/updates/addDiamondsTo1155Categories.ts
@@ -0,0 +1,107 @@
+import { ethers, network } from "hardhat";
+import { gasPrice } from "../helperFunctions";
+import { sendToMultisig } from "../libraries/multisig/multisig";
+const { LedgerSigner } = require("@ethersproject/hardware-wallets");
+
+/* TODO: replace follwowing values */
+const installationDiamond = "0x2953399124F0cBB46d2CbACD8A89cF0599974963";
+const installationTypes = [0, 1];
+const tileDiamond = "0x631998e91476DA5B870D741192fc5Cbc55F5a52E";
+const tileTypes = [0, 1, 2, 3];
+/* TODO: end */
+
+async function main() {
+  const diamondAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
+  let signer;
+
+  const testing = ["hardhat", "localhost"].includes(network.name);
+  if (testing) {
+    const itemManager = "0x8D46fd7160940d89dA026D59B2e819208E714E82";
+    await network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [itemManager],
+    });
+    signer = await ethers.provider.getSigner(itemManager);
+  } else if (network.name === "matic") {
+    signer = new LedgerSigner(ethers.provider, "hid", "m/44'/60'/2'/0/0");
+    console.log("signer:", signer);
+  } else {
+    throw Error("Incorrect network selected");
+  }
+
+  let erc1155MarketplaceFacet = await ethers.getContractAt(
+    "ERC1155MarketplaceFacet",
+    diamondAddress,
+    signer
+  );
+  let tx;
+  let receipt;
+
+  const categories = [];
+  // adding installation type categories
+  for (let i = 0; i < installationTypes.length; i++) {
+    categories.push({
+      erc1155TokenAddress: installationDiamond,
+      erc1155TypeId: installationTypes[i],
+      category: 4,
+    });
+  }
+  // adding tile type categories
+  for (let i = 0; i < tileTypes.length; i++) {
+    categories.push({
+      erc1155TokenAddress: tileDiamond,
+      erc1155TypeId: tileTypes[i],
+      category: 5,
+    });
+  }
+
+  if (testing) {
+    tx = await erc1155MarketplaceFacet.setERC1155Categories(categories);
+    console.log(
+      "Adding categories for both installation and tile diamond tx:",
+      tx.hash
+    );
+    receipt = await tx.wait();
+    if (!receipt.status) {
+      throw Error(`Adding categories failed: ${tx.hash}`);
+    }
+    console.log("Adding categories succeeded:", tx.hash);
+
+    console.log("Checking installation diamond categories...");
+    for (let i = 0; i < installationTypes.length; i++) {
+      const category = await erc1155MarketplaceFacet.getERC1155Category(
+        installationDiamond,
+        installationTypes[i]
+      );
+      console.log(category.eq(4) ? "correct" : "incorrect");
+    }
+    console.log("Checking installation diamond categories...");
+    for (let i = 0; i < tileTypes.length; i++) {
+      const category = await erc1155MarketplaceFacet.getERC1155Category(
+        tileDiamond,
+        tileTypes[i]
+      );
+      console.log(category.eq(5) ? "correct" : "incorrect");
+    }
+  } else {
+    try {
+      tx =
+        await erc1155MarketplaceFacet.populateTransaction.setERC1155Categories(
+          categories,
+          { gasPrice: gasPrice }
+        );
+      console.log("tx:", tx);
+      //@ts-ignore
+      await sendToMultisig(process.env.DIAMOND_UPGRADER, signer, tx, ethers);
+    } catch (error) {
+      console.log("error:", error);
+    }
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
