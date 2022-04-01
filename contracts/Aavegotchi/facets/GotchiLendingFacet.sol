89edf1272f0bead1c112e2b15fe8d348f9cc3791
Patrick <pakim249@ischool.berkeley.edu>
2022-03-31 23:27:21 -0700

    Update max lending period and give already lent gotchis an out after 30 days

--- contracts/Aavegotchi/facets/GotchiLendingFacet.sol
+++ contracts/Aavegotchi/facets/GotchiLendingFacet.sol
@@ -118,7 +118,7 @@ contract GotchiLendingFacet is Modifiers {
         address sender = LibMeta.msgSender();
         require(_originalOwner != address(0), "GotchiLending: Original owner cannot be zero address");
         require(_period > 0, "GotchiLending: Period should be larger than 0");
-        require(_period <= 31536000, "GotchiLending: Period too long"); //No reason to have a period longer than a year
+        require(_period <= 2_592_000, "GotchiLending: Period too long"); //No reason to have a period longer than 30 days
         require(_revenueSplit[0] + _revenueSplit[1] + _revenueSplit[2] == 100, "GotchiLending: Sum of revenue split should be 100");
         if (_thirdParty == address(0)) {
             require(_revenueSplit[2] == 0, "GotchiLending: Revenue split for invalid thirdParty should be zero");
@@ -259,8 +259,9 @@ contract GotchiLendingFacet is Modifiers {
         address sender = LibMeta.msgSender();
         address lender = lending.lender;
         address borrower = lending.borrower;
+        uint32 lendingPeriod = lending.period < 2_592_000 ? lending.period : 2_592_000;
         require((lender == sender) || (borrower == sender), "GotchiLending: Only lender or borrower can claim and end agreement");
-        require(borrower == sender || lending.timeAgreed + lending.period <= block.timestamp, "GotchiLending: Not allowed during agreement");
+        require(borrower == sender || lending.timeAgreed + lendingPeriod <= block.timestamp, "GotchiLending: Not allowed during agreement");
 
         LibGotchiLending.claimGotchiLending(listingId);
 
