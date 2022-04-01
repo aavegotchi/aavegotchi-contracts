e51cdea9aef39969b3160e5408258ef93c7c19fd
Mark Watson <markjwatson86@gmail.com>
2022-04-01 09:28:37 -0700

    sleeve duplication tests pass

--- scripts/fixes/sleeveDuplication-fix.ts
+++ scripts/fixes/sleeveDuplication-fix.ts
@@ -1,3 +1,7 @@
+//fix IDs - 46(half rekt shirt), 50(gldnXross robe), 54(lama corn shirt),
+//          105(portal mage), 109(rasta shirt), 115(blue hawaiian shirt),
+//          56(aagent shirt), 91(pajama shirt)
+
 import { run } from "hardhat";
 import {
   updateSvgTaskForSvgType,
@@ -5,17 +9,17 @@ import {
 } from "../../scripts/svgHelperFunctions";
 
 async function main() {
-  const ids = [105];
+  const ids = [46, 50, 54, 56, 91, 105, 109, 115];
   const sleeveIds = [19];
 
   const body = await updateSvgTaskForSvgType(ids, "front");
-  const sleeves = await updateSleevesTaskForSvgType(ids, sleeveIds, "front");
+  // const sleeves = await updateSleevesTaskForSvgType(ids, sleeveIds, "front");
 
   // console.log("Body: ", body);
   // console.log("Sleeves: ", sleeves);
 
   await run("updateSvgs", body);
-  await run("updateSvgs", sleeves);
+  // await run("updateSvgs", sleeves);
 }
 
 main()
