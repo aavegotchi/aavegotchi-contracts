3bb76a03c6b0d2a101d3cb3bd98a98e9eb71d9b4
Patrick <pakim249@ischool.berkeley.edu>
2022-04-15 10:51:49 -0700

    Add base files for lending refactor

---
+++ helpers/constants.ts
@@ -0,0 +1,9 @@
+import { ethers, BigNumber } from "ethers";
+
+export const aavegotchiDiamondAddressMatic =
+  "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
+export const fudAddress = "0x403e967b044d4be25170310157cb1a4bf10bdd0f";
+export const fomoAddress = "0x44a6e0be76e1d9620a7f76588e4509fe4fa8e8c8";
+export const alphaAddress = "0x6a3e7c3c6ef65ee26975b12293ca1aad7e1daed2";
+export const kekAddress = "0x42e5e06ef5b90fe15f853f59299fc96259209c5c";
+export const ghstAddress = "0x385eeac5cb85a38a9a07a70c73e0a3271cfb54a7";
