74a5de2976113b98a5ae2b37db7bbafbdad10340
Coderdan <dmathieuva@gmail.com>
2022-04-19 14:51:52 +0800

    prepping xp scripts

---
+++ scripts/airdrops/coreprops/grantXP_agip28_coreprop.ts
@@ -0,0 +1,21 @@
+import { run } from "hardhat";
+import { GrantXPSnapshotTaskArgs } from "../../../tasks/grantXP_snapshot";
+
+async function grantXP() {
+  const taskArgs: GrantXPSnapshotTaskArgs = {
+    proposalId:
+      "0x8b5862698c550b53e57f6ce4628a7a13ed5a4772e541c414e9a27fbcf95cf8c2",
+    propType: "coreprop",
+    batchSize: "500",
+  };
+  await run("grantXP_snapshot", taskArgs);
+}
+
+grantXP()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+exports.grantXP = grantXP;
---
+++ scripts/airdrops/sigprops/grantXP_agip26_sigprop.ts
@@ -0,0 +1,21 @@
+import { run } from "hardhat";
+import { GrantXPSnapshotTaskArgs } from "../../../tasks/grantXP_snapshot";
+
+async function grantXP() {
+  const taskArgs: GrantXPSnapshotTaskArgs = {
+    proposalId:
+      "0x2d7a9e2b9c450c124dc0c03e3aeb6c82de79ca3773616cec4d0efa2625545829",
+    propType: "sigprop",
+    batchSize: "500",
+  };
+  await run("grantXP_snapshot", taskArgs);
+}
+
+grantXP()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+exports.grantXP = grantXP;
