5afaaeecaf98f052a5f38266d232dde1550f72ae
Patrick <pakim249@berkeley.edu>
2022-08-04 08:48:11 -0400

    Add events and change init to use lib

--- contracts/Aavegotchi/init/InitBorrowLimit.sol
+++ contracts/Aavegotchi/init/InitBorrowLimit.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.1;
 
-import {AppStorage} from "../libraries/LibAppStorage.sol";
+import {LibWhitelist} from "../libraries/LibWhitelist.sol";
 
 contract InitBorrowLimit {
-    AppStorage internal s;
-
     // Set borrow limit for whitelist id 0 to 1 gotchi
     function init() external {
-        s.whitelistAccessRights[0][0] = 1;
+        LibWhitelist.setWhitelistAccessRight(0, 0, 1);
     }
 }
