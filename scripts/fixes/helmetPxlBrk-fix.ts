88f929364680b50504d5d461832e5ccaeabd6251
Mark Watson <64367041+mjwatson10@users.noreply.github.com>
2022-09-28 10:57:03 -0700

    helmet pixel breaking fix

---
+++ scripts/fixes/helmetPxlBrk-fix.ts
@@ -0,0 +1,26 @@
+// id - 111 (hazmat hood), 252 (astronaut helmet), 255 (lil bubble helmet)
+
+import { run } from "hardhat";
+import { updateSvgTaskForSvgType } from "../../scripts/svgHelperFunctions";
+
+async function main() {
+  const frontIds = [111, 252, 255];
+  const ids = [111, 252, 255];
+
+  const fixFront = await updateSvgTaskForSvgType(frontIds, "front");
+  const fixLeft = await updateSvgTaskForSvgType(ids, "left");
+  const fixRight = await updateSvgTaskForSvgType(ids, "right");
+
+  await run("updateSvgs", fixFront);
+  await run("updateSvgs", fixLeft);
+  await run("updateSvgs", fixRight);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+exports.helmetPxlBrkFix = main;
