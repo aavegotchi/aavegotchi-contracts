6594b14d15bd6c57756169c82e765c318e795ebe
infiniRekt <infiniRekt@gmail.com>
2022-12-27 19:37:08 -0500

    add upgrade script for Aavegotchi contracts changes

---
+++ scripts/upgrades/upgrade-aavegotchiForForge.ts
@@ -0,0 +1,63 @@
+import { ethers, run } from "hardhat";
+import {
+    convertFacetAndSelectorsToString,
+    DeployUpgradeTaskArgs,
+    FacetsAndAddSelectors,
+} from "../../tasks/deployUpgrade";
+
+import { maticDiamondAddress, maticDiamondUpgrader } from "../helperFunctions";
+
+export async function upgradeAavegotchiForForge() {
+    console.log("Upgrading Aavegotchi facets for Forge.");
+
+    const signerAddress = await (await ethers.getSigners())[0].getAddress();
+
+    const facets: FacetsAndAddSelectors[] = [
+        {
+            facetName:
+                "contracts/Aavegotchi/facets/AavegotchiGameFacet.sol:AavegotchiGameFacet",
+            addSelectors: [
+                "function aavegotchiLocked(uint256 _tokenId) external view returns (bool isLocked)",
+            ],
+            removeSelectors: [],
+        },
+        {
+            facetName:
+                "contracts/Aavegotchi/facets/CollateralFacet.sol:CollateralFacet",
+            addSelectors: [],
+            removeSelectors: [],
+        },
+        {
+            facetName:
+                "contracts/Aavegotchi/facets/DAOFacet.sol:DAOFacet",
+            addSelectors: [
+                "function setForge(address _newForge) external",
+            ],
+            removeSelectors: [],
+        },
+    ];
+
+    const joined = convertFacetAndSelectorsToString(facets);
+
+    const args: DeployUpgradeTaskArgs = {
+        diamondUpgrader: signerAddress,
+        diamondAddress: maticDiamondAddress,
+        facetsAndAddSelectors: joined,
+        useLedger: false,
+        useMultisig: false,
+        freshDeployment: false
+    };
+
+    await run("deployUpgrade", args);
+
+    console.log("Finished upgrading Aavegotchi facets for Forge.");
+}
+
+if (require.main === module) {
+    upgradeAavegotchiForForge()
+        .then(() => process.exit(0))
+        .catch((error) => {
+            console.error(error);
+            process.exit(1);
+        });
+}
