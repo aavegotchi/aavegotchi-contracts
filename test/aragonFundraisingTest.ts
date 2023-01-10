6799fb35c2dc69c6f8dab1fed27ec03818fba140
orionstardust <justinono1990@outlook.com>
2023-01-10 12:31:24 -0800

    Add test case for unclaimed orders

--- test/aragonFundraisingTest.ts
+++ test/aragonFundraisingTest.ts
@@ -4,6 +4,7 @@ import {
   ACL,
   Agent,
   AragonFundraisingController,
+  BatchedBancorMarketMaker,
   IERC20,
   IKernel,
   Voting,
@@ -11,13 +12,16 @@ import {
 import { encodeCallScript } from "@aragon/contract-helpers-test/src/aragon-os";
 import { expect } from "chai";
 import { utils } from "ethers";
+import { BigNumberish } from "@ethersproject/bignumber";
 
 const reserveAddress = "0xfB76E9be55758d0042e003c1E46E186360F0627e"; // Treasury
 const fundraisingControllerAddress =
   "0xe5ECFB44bccd7A585fA7F4a8E02C450e525AF2E4";
 const collateralAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"; // DAI
-const sellerAddress = "0xC97252f98B7159251D410fE237674dC5bF7465A1"; // DAI, GHST Holder
-const buyerAddress = "0x0339BBf78947CeA3D07DA41493B8F0be541f78dc"; // DAI, GHST Holder
+const ghstAddress = "0x3F382DbD960E3a9bbCeaE22651E88158d2791550"; // GHST
+
+const sellerAddress = "0x50C114E2f3fA1DaFCfbE675b342f5868bf1727f0"; // GHST Holder
+const buyerAddress = "0x0339BBf78947CeA3D07DA41493B8F0be541f78dc"; // DAI Holder
 
 // Voting
 const collateralManagerAddress = "0xF63e1edbcb3BE8d5fB124F4A228F5412f48E5ae7"; // proxy of voting contract
@@ -47,8 +51,16 @@ describe("Aragon Fundraising Close Test", async function () {
   let votingContract: Voting;
   let votingContractWithCreator: Voting;
   let collateral: IERC20;
+  let ghst: IERC20;
+  let marketMaker: BatchedBancorMarketMaker;
 
   let marketMakerAddress: string;
+  let buyOrderBatchId: BigNumberish;
+  let sellOrderBatchId: BigNumberish;
+  let sellerGhstBalAfterOrder: BigNumberish;
+  let sellerCollateralBalAfterOrder: BigNumberish;
+  let buyerGhstBalAfterOrder: BigNumberish;
+  let buyerCollateralBalAfterOrder: BigNumberish;
 
   before(async function () {
     fundraisingController = (await ethers.getContractAt(
@@ -90,8 +102,16 @@ describe("Aragon Fundraising Close Test", async function () {
       "contracts/shared/interfaces/IERC20.sol:IERC20",
       collateralAddress
     )) as IERC20;
+    ghst = (await ethers.getContractAt(
+      "contracts/shared/interfaces/IERC20.sol:IERC20",
+      ghstAddress
+    )) as IERC20;
 
     marketMakerAddress = await fundraisingController.marketMaker();
+    marketMaker = (await ethers.getContractAt(
+      "BatchedBancorMarketMaker",
+      marketMakerAddress
+    )) as BatchedBancorMarketMaker;
 
     votingContract = (await ethers.getContractAt(
       "Voting",
@@ -124,6 +144,10 @@ describe("Aragon Fundraising Close Test", async function () {
   });
 
   it("Order should be succeeded before removing collateral", async function () {
+    const sellerGhstBalBeforeOrder = await ghst.balanceOf(sellerAddress);
+    const sellerCollateralBalBeforeOrder = await collateral.balanceOf(
+      sellerAddress
+    );
     let receipt = await (
       await fundraisingControllerWithSeller.openSellOrder(
         collateralAddress,
@@ -135,7 +159,21 @@ describe("Aragon Fundraising Close Test", async function () {
       (event) => event.topics && event.topics[0] === topic
     );
     expect(event!.address).to.equal(marketMakerAddress);
+    sellOrderBatchId = await marketMaker.getCurrentBatchId();
 
+    sellerGhstBalAfterOrder = await ghst.balanceOf(sellerAddress);
+    sellerCollateralBalAfterOrder = await collateral.balanceOf(sellerAddress);
+    expect(sellerGhstBalAfterOrder.add(orderValue)).to.equal(
+      sellerGhstBalBeforeOrder
+    );
+    expect(sellerCollateralBalAfterOrder).to.equal(
+      sellerCollateralBalBeforeOrder
+    );
+
+    const buyerGhstBalBeforeOrder = await ghst.balanceOf(buyerAddress);
+    const buyerCollateralBalBeforeOrder = await collateral.balanceOf(
+      buyerAddress
+    );
     receipt = await (
       await fundraisingControllerWithBuyer.openBuyOrder(
         collateralAddress,
@@ -147,6 +185,14 @@ describe("Aragon Fundraising Close Test", async function () {
       (event) => event.topics && event.topics[0] === topic
     );
     expect(event!.address).to.equal(marketMakerAddress);
+    buyOrderBatchId = await marketMaker.getCurrentBatchId();
+
+    buyerGhstBalAfterOrder = await ghst.balanceOf(buyerAddress);
+    buyerCollateralBalAfterOrder = await collateral.balanceOf(buyerAddress);
+    expect(
+      buyerCollateralBalBeforeOrder.sub(buyerCollateralBalAfterOrder).gt(0)
+    ).to.equal(true);
+    expect(buyerGhstBalAfterOrder).to.equal(buyerGhstBalBeforeOrder);
   });
 
   it("Order should be failed after removing collateral", async function () {
@@ -174,12 +220,50 @@ describe("Aragon Fundraising Close Test", async function () {
       );
       await (await votingContract.vote(voteId, true, true)).wait();
     }
-    // await (
-    //   await fundraisingControllerWithCollateralManager.removeCollateralToken(
-    //     collateralAddress
-    //   )
-    // ).wait();
 
+    // check buyer and seller balances of ghst and collateral after removing collateral
+    const sellerGhstBalAfterRemove = await ghst.balanceOf(sellerAddress);
+    const sellerCollateralBalAfterRemove = await collateral.balanceOf(
+      sellerAddress
+    );
+    const buyerGhstBalAfterRemove = await ghst.balanceOf(buyerAddress);
+    const buyerCollateralBalAfterRemove = await collateral.balanceOf(
+      buyerAddress
+    );
+    expect(sellerGhstBalAfterRemove).to.equal(sellerGhstBalAfterOrder);
+    expect(sellerCollateralBalAfterRemove).to.equal(
+      sellerCollateralBalAfterOrder
+    );
+    expect(buyerGhstBalAfterRemove).to.equal(buyerGhstBalAfterOrder);
+    expect(buyerCollateralBalAfterRemove).to.equal(
+      buyerCollateralBalAfterOrder
+    );
+
+    const buyBatch = await marketMaker.getBatch(
+      buyOrderBatchId,
+      collateralAddress
+    );
+    const sellBatch = await marketMaker.getBatch(
+      sellOrderBatchId,
+      collateralAddress
+    );
+    expect(buyBatch[1]).to.equal(false); // batch is canceled?
+    expect(sellBatch[1]).to.equal(false); // batch is canceled?
+
+    await expect(
+      fundraisingControllerWithBuyer.claimBuyOrder(
+        buyerAddress,
+        buyOrderBatchId,
+        collateralAddress
+      )
+    ).to.be.revertedWith("MM_COLLATERAL_NOT_WHITELISTED");
+    await expect(
+      fundraisingControllerWithSeller.claimSellOrder(
+        buyerAddress,
+        buyOrderBatchId,
+        collateralAddress
+      )
+    ).to.be.revertedWith("MM_COLLATERAL_NOT_WHITELISTED");
     await expect(
       fundraisingControllerWithSeller.openSellOrder(
         collateralAddress,
@@ -235,7 +319,10 @@ describe("Aragon Fundraising Close Test", async function () {
 
     // transfer collateral after getting roles
     const balanceBefore = await collateral.balanceOf(reserveAddress);
-    console.log(utils.formatEther(balanceBefore));
+    // console.log(
+    //   "Collateral balance before transfer",
+    //   utils.formatEther(balanceBefore)
+    // );
     await (
       await agentWithWithdrawer.transfer(
         collateralAddress,
@@ -245,7 +332,10 @@ describe("Aragon Fundraising Close Test", async function () {
     ).wait();
 
     const balanceAfter = await collateral.balanceOf(reserveAddress);
-    console.log(utils.formatEther(balanceAfter));
+    // console.log(
+    //   "Collateral balance after transfer",
+    //   utils.formatEther(balanceAfter)
+    // );
     expect(balanceAfter).to.equal(0);
   });
 });
