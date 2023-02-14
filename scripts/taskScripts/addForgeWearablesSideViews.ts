980d2a14e5a829d7769eec08495b68cf59318a60
orionstardust <justinono1990@outlook.com>
2023-02-14 04:13:03 -0800

    Fix an issue

--- scripts/taskScripts/addForgeWearablesSideViews.ts
+++ scripts/taskScripts/addForgeWearablesSideViews.ts
@@ -13,7 +13,11 @@ import { convertSideDimensionsToTaskFormat } from "../../tasks/updateItemSideDim
 import { convertExceptionsToTaskFormat } from "../../tasks/updateWearableExceptions";
 
 export async function addForgeWearableSideViews() {
-  const itemIds: number[] = [];
+  const itemIds: number[] = [
+    316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330,
+    331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345,
+    346, 347, 348, 349,
+  ];
   for (let index = 0; index < itemTypes1.length; index++) {
     itemIds.push(Number(itemTypes1[index].svgId));
   }
@@ -23,14 +27,14 @@ export async function addForgeWearableSideViews() {
 
   // uploading svg files
   let sideViewsTaskArray = await updateSvgTaskForSideViews(itemIds);
-  console.log("sideViewsTaskArray", sideViewsTaskArray);
+  // console.log("sideViewsTaskArray", sideViewsTaskArray);
   for (let index = 0; index < sideViewsTaskArray.length; index++) {
     await run("updateSvgs", sideViewsTaskArray[index]);
   }
 
   const sleeveIds = [52, 53, 54];
   let sleevesTaskArray = await updateSvgTaskForSideSleeves(sleeveIds);
-  console.log("sleevesTaskArray", sleevesTaskArray);
+  // console.log("sleevesTaskArray", sleevesTaskArray);
   for (let index = 0; index < sleevesTaskArray.length; index++) {
     await run("updateSvgs", sleevesTaskArray[index]);
   }
@@ -41,7 +45,7 @@ export async function addForgeWearableSideViews() {
     convertSideDimensionsToTaskFormat(sideViewDimensions)
   );
 
-  // uploading svg excpetions
+  // uploading svg exceptions
   await run(
     "updateWearableExceptions",
     convertExceptionsToTaskFormat(forgeSideExceptions)
