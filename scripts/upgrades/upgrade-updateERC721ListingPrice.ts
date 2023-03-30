48f735aec65ebf106bc267482f2f01b259f5cc29
Coderdan <dmathieuva@gmail.com>
2023-03-30 19:41:43 +0800

    Update upgrade-updateERC721ListingPrice.ts

--- scripts/upgrades/upgrade-updateERC721ListingPrice.ts
+++ scripts/upgrades/upgrade-updateERC721ListingPrice.ts
@@ -33,7 +33,7 @@ export async function upgrade() {
     diamondAddress: maticDiamondAddress,
     facetsAndAddSelectors: joined,
     useLedger: true,
-    useMultisig: true,
+    useMultisig: false,
   };
 
   await run("deployUpgrade", args);
