8a9abaa2e28035c36be2ccdb63070c9d1ae49550
kevinkons <kevin.kons@hotmail.com>
2023-06-27 23:55:44 -0300

    inspect script

---
+++ scripts/gotchichainBridge/items/inspectItemMumbaiSide.ts
@@ -0,0 +1,27 @@
+/* global ethers hre */
+
+import { ethers } from "hardhat";
+
+const aavegotchDiamondAddressMumbai = process.env.AAVEGOTCHI_DIAMOND_ADDRESS_MUMBAI as string
+
+export default async function main(to: string) {
+  const itemsFacetPolygonSide = await ethers.getContractAt("contracts/Aavegotchi/facets/ItemsFacet.sol:ItemsFacet", aavegotchDiamondAddressMumbai)
+
+  const itemBalances = await itemsFacetPolygonSide.itemBalances(to) 
+
+  console.log(`Item balances for ${to}`, itemBalances.toString());
+  console.log(itemBalances)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+if (require.main === module) {
+  main("to")
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
+
+exports.deployProject = main;
