1b5b997d2a0f1750c957dae8f60ebc6852734f86
kevinkons <kevin.kons@hotmail.com>
2023-07-12 15:32:26 -0300

    adding daoFacet call to setup bridge script

--- scripts/gotchichainBridge/aavegotchis/setupBridgeGotchichainSide.ts
+++ scripts/gotchichainBridge/aavegotchis/setupBridgeGotchichainSide.ts
@@ -5,23 +5,33 @@ import { ethers } from "hardhat";
 const lzChainIdMumbai = process.env.LZ_CHAIN_ID_MUMBAI as string
 const aavegotchiBridgeAddressMumbai = process.env.AAVEGOTCHI_BRIDGE_ADDRESS_MUMBAI as string
 const aavegotchiBridgeAddressGotchichain = process.env.AAVEGOTCHI_BRIDGE_ADDRESS_GOTCHICHAIN as string
+const aavegotchDiamondAddressGotchichain = process.env.AAVEGOTCHI_DIAMOND_ADDRESS_GOTCHICHAIN as string
+
+const txParams = {
+  gasPrice: "5000000000"
+}
 
 export default async function main() {
   const bridgeGotchichainSide = await ethers.getContractAt("AavegotchiBridgeGotchichainSide", aavegotchiBridgeAddressGotchichain)
-
-  let tx = await bridgeGotchichainSide.setTrustedRemote(lzChainIdMumbai, ethers.utils.solidityPack(["address", "address"], [aavegotchiBridgeAddressMumbai, bridgeGotchichainSide.address]))
+  const daoFacetGotchichainSide = await ethers.getContractAt("DAOFacet", aavegotchDiamondAddressGotchichain)
+  
+  let tx = await bridgeGotchichainSide.setTrustedRemote(lzChainIdMumbai, ethers.utils.solidityPack(["address", "address"], [aavegotchiBridgeAddressMumbai, bridgeGotchichainSide.address]), txParams)
   console.log(`Wating for tx to be validated, tx hash: ${tx.hash}`)
   await tx.wait()
 
-  tx = await bridgeGotchichainSide.setDstChainIdToBatchLimit(lzChainIdMumbai, 1)
+  tx = await bridgeGotchichainSide.setDstChainIdToBatchLimit(lzChainIdMumbai, 1, txParams)
   console.log(`Wating for tx to be validated, tx hash: ${tx.hash}`)
   await tx.wait()
 
-  tx = await bridgeGotchichainSide.setMinDstGas(lzChainIdMumbai, 1, 35000)
+  tx = await bridgeGotchichainSide.setMinDstGas(lzChainIdMumbai, 1, 35000, txParams)
   console.log(`Wating for tx to be validated, tx hash: ${tx.hash}`)
   await tx.wait()
 
-  tx = await bridgeGotchichainSide.setDstChainIdToTransferGas(lzChainIdMumbai, 1950000)
+  tx = await bridgeGotchichainSide.setDstChainIdToTransferGas(lzChainIdMumbai, 1950000, txParams)
+  console.log(`Wating for tx to be validated, tx hash: ${tx.hash}`)
+  await tx.wait()
+
+  tx = await daoFacetGotchichainSide.addLayerZeroBridgeAddress(bridgeGotchichainSide.address, txParams)
   console.log(`Wating for tx to be validated, tx hash: ${tx.hash}`)
   await tx.wait()
 
