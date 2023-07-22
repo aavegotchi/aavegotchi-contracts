c4efc12c78ea304e5bb27caa121b68bd49bb30ab
Narayan Prusty <arsenal.narayan@gmail.com>
2023-07-22 20:41:22 +0100

    Revert "revert commented out code"
    This reverts commit 5fa8a57fd059640d15ad4bde909397d87ab6802b.

--- contracts/Aavegotchi/facets/AavegotchiGameFacet.sol
+++ contracts/Aavegotchi/facets/AavegotchiGameFacet.sol
@@ -244,7 +244,7 @@ contract AavegotchiGameFacet is Modifiers {
         address escrow = address(new CollateralEscrow(option.collateralType));
         aavegotchi.escrow = escrow;
         address owner = LibMeta.msgSender();
-        LibERC20.transferFrom(option.collateralType, owner, escrow, _stakeAmount);
+        // LibERC20.transferFrom(option.collateralType, owner, escrow, _stakeAmount);
         LibERC721Marketplace.cancelERC721Listing(address(this), _tokenId, owner);
     }
 
