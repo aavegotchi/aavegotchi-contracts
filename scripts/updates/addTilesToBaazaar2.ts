ed85c4096e6d6ede9832253adf9daee95e27da66
Timidan <akinnusotutemitayodaniel@gmail.com>
2023-12-20 21:41:14 +0100

    fix ids

--- scripts/updates/addTilesToBaazaar2.ts
+++ scripts/updates/addTilesToBaazaar2.ts
@@ -5,7 +5,7 @@ import { LedgerSigner } from "@anders-t/ethers-ledger";
 //all remaining tiles
 const tileIds = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
 ];
 const category = 5;
 
