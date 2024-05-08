f047aa38c1ce4a9d94df6c7857be14f77bf01855
orionstardust <justinono1990@outlook.com>
2024-05-08 00:05:52 -0400

    add upgrade scrupt for batch escrow

---
+++ scripts/upgrades/upgrade-batchEscrow.ts
@@ -0,0 +1,50 @@
+import { run } from "hardhat";
+import {
+  convertFacetAndSelectorsToString,
+  DeployUpgradeTaskArgs,
+  FacetsAndAddSelectors,
+} from "../../tasks/deployUpgrade";
+
+import { maticDiamondAddress, maticDiamondUpgrader } from "../helperFunctions";
+
+export async function upgradeBatchEscrow() {
+  console.log("Upgrading Aavegotchi facets for batch escrow.");
+
+  const facets: FacetsAndAddSelectors[] = [
+    {
+      facetName:
+        "contracts/Aavegotchi/facets/EscrowFacet.sol:EscrowFacet",
+      addSelectors: [
+        "function batchTransferEscrow(uint256[] calldata _tokenIds,address[] calldata _erc20Contracts,address[] calldata _recipients,uint256[] calldata _transferAmounts) external",
+      ],
+      removeSelectors: [],
+    },
+  ];
+
+  const joined = convertFacetAndSelectorsToString(facets);
+
+  const args: DeployUpgradeTaskArgs = {
+    // diamondUpgrader: maticDiamondUpgrader,
+    diamondUpgrader: maticDiamondUpgrader,
+    diamondAddress: maticDiamondAddress,
+    facetsAndAddSelectors: joined,
+    useLedger: true,
+    useMultisig: false,
+    freshDeployment: false,
+    // initAddress: maticDiamondAddress,
+    // initCalldata: calldata,
+  };
+
+  await run("deployUpgrade", args);
+
+  console.log("Finished upgrading Aavegotchi facets.");
+}
+
+if (require.main === module) {
+  upgradeBatchEscrow()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
