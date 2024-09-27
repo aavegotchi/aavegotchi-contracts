f1327d68c45518976b611e5da9a2322bd59efbdc
orionstardust <justinono1990@outlook.com>
2024-09-26 22:44:19 -0400

    add script for configuring socket bridges on diamonds

---
+++ scripts/geistBridge/configureBridge.ts
@@ -0,0 +1,61 @@
+/* global ethers hre */
+
+import { ethers, network } from "hardhat";
+import { maticDiamondAddress } from "../helperFunctions";
+
+export default async function main() {
+  let gotchiBridgeAddress;
+  let itemBridgeAddress;
+  let diamondAddress;
+  if (network.name === "polter") {
+    diamondAddress = "0x6b54b36A54b068152f0f39FdA0Bf96e02176D95B"
+    // controller address
+    gotchiBridgeAddress = "0xEa4BE882A0105E44DEF336F8B2d4FB2E317e6877"
+    itemBridgeAddress = "0xEa4BE882A0105E44DEF336F8B2d4FB2E317e6877"
+  } else if (network.name === "amoy") {
+    diamondAddress = "0xC80DB01aeDAD5F6E3088c75F60E52f579Cf1D3Cb"
+    // vault address
+    gotchiBridgeAddress = "0xfc1a9d9898e7a48D75EF6f18F5c042f8fc2E9055"
+    itemBridgeAddress = "0xfc1a9d9898e7a48D75EF6f18F5c042f8fc2E9055"
+  } else if (network.name === "matic") {
+    diamondAddress = maticDiamondAddress
+    // TODO: vault address
+    gotchiBridgeAddress = ""
+    itemBridgeAddress = ""
+
+  } else if (network.name === "geist") {
+    diamondAddress = ""
+    // TODO: controller address
+    gotchiBridgeAddress = ""
+    itemBridgeAddress = ""
+  } else {
+    throw Error("No network settings for " + network.name);
+  }
+
+  const daoFacet = await ethers.getContractAt("DAOFacet", diamondAddress)
+
+  console.log(`Configuring gotchi bridge...`)
+  let tx = await daoFacet.updateGotchiGeistBridge("0xEa4BE882A0105E44DEF336F8B2d4FB2E317e6877")
+  console.log(`Wating for tx to be validated, tx hash: ${tx.hash}`)
+  await tx.wait()
+
+  console.log(`Configuring items bridge...`)
+  tx = await daoFacet.updateItemGeistBridge("0xEa4BE882A0105E44DEF336F8B2d4FB2E317e6877")
+  console.log(`Wating for tx to be validated, tx hash: ${tx.hash}`)
+  await tx.wait()
+
+  console.log(`Bridge configured on ${network.name}.`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+if (require.main === module) {
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
+
+exports.deployProject = main;
