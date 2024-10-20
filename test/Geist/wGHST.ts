889fe045d3589c2bc861fc3a73b203e66df822b6
Coderdan <dmathieuva@gmail.com>
2024-10-20 17:48:27 +0800

    chore: use solmate weth contract

--- test/Geist/wGHST.ts
+++ test/Geist/wGHST.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { constants, Contract, utils } from "ethers";
+import { constants, Contract } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("WGHST", function () {
@@ -8,6 +8,8 @@ describe("WGHST", function () {
   let owner: SignerWithAddress;
   let addr1: SignerWithAddress;
   let addr2: SignerWithAddress;
+
+  const amount = ethers.utils.parseEther("1");
 
   beforeEach(async function () {
     [owner, addr1, addr2] = await ethers.getSigners();
@@ -23,39 +25,38 @@ describe("WGHST", function () {
   });
 
   it("Should allow deposits and withdrawals", async function () {
-    const depositAmount = ethers.utils.parseEther("1");
-    await wghst.deposit({ value: depositAmount });
-    expect(await wghst.balanceOf(owner.address)).to.equal(depositAmount);
+    await wghst.deposit({ value: amount });
+    expect(await wghst.balanceOf(owner.address)).to.equal(amount);
 
-    await wghst.withdraw(depositAmount);
+    await wghst.withdraw(amount);
     expect(await wghst.balanceOf(owner.address)).to.equal(0);
   });
 
   it("Should emit Deposit and Withdrawal events", async function () {
-    const depositAmount = ethers.utils.parseEther("1");
-    await expect(wghst.deposit({ value: depositAmount }))
+    await expect(wghst.deposit({ value: amount }))
       .to.emit(wghst, "Deposit")
-      .withArgs(owner.address, depositAmount);
+      .withArgs(owner.address, amount);
 
-    await expect(wghst.withdraw(depositAmount))
+    await expect(wghst.withdraw(amount))
       .to.emit(wghst, "Withdrawal")
-      .withArgs(owner.address, depositAmount);
+      .withArgs(owner.address, amount);
   });
 
-  // it("Should handle receive function", async function () {
-  //   const depositAmount = ethers.utils.parseEther("1");
-  //   await owner.sendTransaction({ to: wghst.address, value: depositAmount });
-  //   expect(await wghst.balanceOf(owner.address)).to.equal(depositAmount);
-  // });
+  it("Should handle receive function", async function () {
+    await owner.sendTransaction({ to: wghst.address, value: amount });
+
+    expect(await wghst.balanceOf(owner.address)).to.equal(amount);
+  });
 
   it("Should return correct total supply", async function () {
-    const depositAmount = ethers.utils.parseEther("1");
-    await wghst.deposit({ value: depositAmount });
-    expect(await wghst.totalSupply()).to.equal(depositAmount);
+    await wghst.deposit({ value: amount });
+    await wghst.deposit({ value: amount });
+    await wghst.deposit({ value: amount });
+
+    expect(await wghst.totalSupply()).to.equal(amount.mul(3));
   });
 
   it("Should allow approvals and transfers", async function () {
-    const amount = ethers.utils.parseEther("1");
     await wghst.deposit({ value: amount });
 
     await expect(wghst.approve(addr1.address, amount))
@@ -74,7 +75,6 @@ describe("WGHST", function () {
   });
 
   it("Should allow transferFrom", async function () {
-    const amount = ethers.utils.parseEther("1");
     await wghst.deposit({ value: amount });
     await wghst.approve(addr1.address, amount);
 
@@ -89,7 +89,6 @@ describe("WGHST", function () {
   });
 
   it("Should handle max approval correctly", async function () {
-    const amount = ethers.utils.parseEther("1");
     await wghst.deposit({ value: amount });
     await wghst.approve(addr1.address, ethers.constants.MaxUint256);
 
@@ -102,89 +101,81 @@ describe("WGHST", function () {
   });
 
   it("Should revert on insufficient balance", async function () {
-    const amount = ethers.utils.parseEther("1");
-    await expect(wghst.withdraw(amount)).to.be.revertedWithCustomError(
-      wghst,
-      "InsufficientBalance"
-    );
-    await expect(
-      wghst.transfer(addr1.address, amount)
-    ).to.be.revertedWithCustomError(wghst, "InsufficientBalance");
-    await expect(
-      wghst.transferFrom(owner.address, addr1.address, amount)
-    ).to.be.revertedWithCustomError(wghst, "InsufficientBalance");
+    await expect(wghst.withdraw(amount)).to.be.reverted;
+    await expect(wghst.transfer(addr1.address, amount)).to.be.reverted;
+    await expect(wghst.transferFrom(owner.address, addr1.address, amount)).to.be
+      .reverted;
   });
 
   it("Should revert on insufficient allowance", async function () {
-    const amount = ethers.utils.parseEther("1");
     await wghst.deposit({ value: amount });
     await expect(
       wghst.connect(addr1).transferFrom(owner.address, addr2.address, amount)
-    ).to.be.revertedWithCustomError(wghst, "InsufficientAllowance");
+    ).to.be.reverted;
   });
 
-  it("should revert on permit", async function () {
-    // set the domain parameters
-    const domain = {
-      name: await wghst.name(),
-      version: "1",
-      chainId: 1,
-      verifyingContract: wghst.address,
-    };
+  // it("should revert on permit", async function () {
+  //   // set the domain parameters
+  //   const domain = {
+  //     name: await wghst.name(),
+  //     version: "1",
+  //     chainId: 1,
+  //     verifyingContract: wghst.address,
+  //   };
 
-    // set the Permit type parameters
-    const types = {
-      Permit: [
-        {
-          name: "owner",
-          type: "address",
-        },
-        {
-          name: "spender",
-          type: "address",
-        },
-        {
-          name: "value",
-          type: "uint256",
-        },
-        {
-          name: "nonce",
-          type: "uint256",
-        },
-        {
-          name: "deadline",
-          type: "uint256",
-        },
-      ],
-    };
+  //   // set the Permit type parameters
+  //   const types = {
+  //     Permit: [
+  //       {
+  //         name: "owner",
+  //         type: "address",
+  //       },
+  //       {
+  //         name: "spender",
+  //         type: "address",
+  //       },
+  //       {
+  //         name: "value",
+  //         type: "uint256",
+  //       },
+  //       {
+  //         name: "nonce",
+  //         type: "uint256",
+  //       },
+  //       {
+  //         name: "deadline",
+  //         type: "uint256",
+  //       },
+  //     ],
+  //   };
 
-    // set the Permit type values
-    const values = {
-      owner: owner.address,
-      spender: addr1.address,
-      value: ethers.utils.parseEther("1"),
-      nonce: 0,
-      deadline: 0,
-    };
+  //   // set the Permit type values
+  //   const values = {
+  //     owner: owner.address,
+  //     spender: addr1.address,
+  //     value: ethers.utils.parseEther("1"),
+  //     nonce: 0,
+  //     deadline: 0,
+  //   };
 
-    // sign the Permit type data with the deployer's private key
-    const signature = await owner._signTypedData(domain, types, values);
+  //   // sign the Permit type data with the deployer's private key
+  //   const signature = await owner._signTypedData(domain, types, values);
 
-    // split the signature into its components
-    const sig = ethers.utils.splitSignature(signature);
+  //   // split the signature into its components
+  //   const sig = ethers.utils.splitSignature(signature);
 
-    await expect(
-      wghst.permit(
-        owner.address,
-        addr1.address,
-        ethers.utils.parseEther("1"),
-        0,
-        sig.v,
-        sig.r,
-        sig.s
-      )
-    ).to.be.revertedWithCustomError(wghst, "PermitNotSupported");
-  });
+  //   await expect(
+  //     wghst.permit(
+  //       owner.address,
+  //       addr1.address,
+  //       ethers.utils.parseEther("1"),
+  //       0,
+  //       sig.v,
+  //       sig.r,
+  //       sig.s
+  //     )
+  //   ).to.be.revertedWithCustomError(wghst, "PermitNotSupported");
+  // });
 
   it("should revert on transfer to 0 address", async function () {
     await expect(
@@ -267,6 +258,6 @@ describe("WGHST", function () {
         sig.r,
         sig.s
       )
-    ).to.be.revertedWithCustomError(wghst, "PermitNotSupported");
+    ).to.be.reverted;
   });
 });
