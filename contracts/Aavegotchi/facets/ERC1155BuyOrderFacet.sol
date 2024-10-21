b625fcceb15ca6d89a3a88eb396133c03312f6a5
Coderdan <dmathieuva@gmail.com>
2024-10-21 19:31:48 +0800

    Update contracts/Aavegotchi/facets/ERC1155BuyOrderFacet.sol
    Co-authored-by: Timidan <akinnusotutemitayodaniel@gmail.com>

--- contracts/Aavegotchi/facets/ERC1155BuyOrderFacet.sol
+++ contracts/Aavegotchi/facets/ERC1155BuyOrderFacet.sol
@@ -50,7 +50,7 @@ contract ERC1155BuyOrderFacet is Modifiers {
         uint256 category = LibSharedMarketplace.getERC1155Category(_erc1155TokenAddress, _erc1155TokenId);
 
         // Transfer ETH
-        require(msg.value == cost, "ERC1155BuyOrder: Not enough GHST sent");
+        require(msg.value == cost, "ERC1155BuyOrder:GHST amount mismatch");
 
         // Place new buy order
         s.nextERC1155BuyOrderId++;
