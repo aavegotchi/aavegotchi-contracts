480c5711c7d446b4e6118d6a5cfbfbfe13649660
Coderdan <dmathieuva@gmail.com>
2024-10-21 19:32:00 +0800

    Update contracts/Aavegotchi/facets/ERC1155MarketplaceFacet.sol
    Co-authored-by: Timidan <akinnusotutemitayodaniel@gmail.com>

--- contracts/Aavegotchi/facets/ERC1155MarketplaceFacet.sol
+++ contracts/Aavegotchi/facets/ERC1155MarketplaceFacet.sol
@@ -256,7 +256,7 @@ contract ERC1155MarketplaceFacet is Modifiers {
         uint256 cost = _quantity * _priceInWei;
 
         // Transfer ETH from buyer to contract
-        require(msg.value == cost, "ERC1155Marketplace: Insufficient GHST sent");
+        require(msg.value == cost, "ERC1155Marketplace: GHST amount mismtch");
 
         {
             BaazaarSplit memory split = LibSharedMarketplace.getBaazaarSplit(
