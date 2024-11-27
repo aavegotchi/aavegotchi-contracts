69e09a346defe230ccb1d670cfed47a364ff75c1
Coderdan <dmathieuva@gmail.com>
2024-11-27 22:35:13 +0800

    chore: revert collateral changes

--- contracts/Aavegotchi/facets/AavegotchiGameFacet.sol
+++ contracts/Aavegotchi/facets/AavegotchiGameFacet.sol
@@ -229,6 +229,7 @@ contract AavegotchiGameFacet is Modifiers {
     ///@dev If the NFT(Portal) with identifier `_tokenId` is listed for sale on the baazaar while it is being unlocked, that listing is cancelled
     ///@param _tokenId The identifier of NFT to claim an Aavegotchi from
     ///@param _option The index of the aavegotchi to claim(1-10)
+    ///@param _stakeAmount Minimum amount of collateral tokens needed to be sent to the new aavegotchi escrow contract
     function claimAavegotchi(uint256 _tokenId, uint256 _option, uint256 _stakeAmount) external onlyUnlocked(_tokenId) onlyAavegotchiOwner(_tokenId) {
         Aavegotchi storage aavegotchi = s.aavegotchis[_tokenId];
         require(aavegotchi.status == LibAavegotchi.STATUS_OPEN_PORTAL, "AavegotchiGameFacet: Portal not open");
@@ -253,9 +254,7 @@ contract AavegotchiGameFacet is Modifiers {
         address escrow = address(new CollateralEscrow(option.collateralType));
         aavegotchi.escrow = escrow;
         address owner = LibMeta.msgSender();
-
         LibERC20.transferFrom(option.collateralType, owner, escrow, _stakeAmount);
-
         LibERC721Marketplace.cancelERC721Listing(address(this), _tokenId, owner);
     }
 
--- contracts/Aavegotchi/facets/CollateralFacet.sol
+++ contracts/Aavegotchi/facets/CollateralFacet.sol
@@ -98,7 +98,9 @@ contract CollateralFacet is Modifiers {
         require(escrow != address(0), "CollateralFacet: Does not have an escrow");
 
         address collateralType = s.aavegotchis[_tokenId].collateralType;
-        require(IERC20(collateralType).balanceOf(escrow) >= _reduceAmount, "CollateralFacet: Not enough collateral to decrease");
+        uint256 currentStake = IERC20(collateralType).balanceOf(escrow);
+        uint256 minimumStake = s.aavegotchis[_tokenId].minimumStake;
+        require(currentStake - _reduceAmount >= minimumStake, "CollateralFacet: Cannot reduce below minimum stake");
 
         emit DecreaseStake(_tokenId, _reduceAmount);
         LibERC20.transferFrom(collateralType, escrow, LibMeta.msgSender(), _reduceAmount);
