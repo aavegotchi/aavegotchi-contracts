9f31cd829aefea50b232db1d1c828d8e24a05d20
Coderdan <dmathieuva@gmail.com>
2025-01-22 22:27:09 +0800

    chore: add scripts

---
+++ data/airdrops/rarityfarming/szn10/rnd2.ts
@@ -0,0 +1,12 @@
+import { RarityFarmingData } from "../../../../types";
+
+const rarity: string[] = [];
+
+const kinship: string[] = [];
+const xp: string[] = [];
+
+export const dataArgs: RarityFarmingData = {
+  rarityGotchis: rarity,
+  kinshipGotchis: kinship,
+  xpGotchis: xp,
+};
---
+++ scripts/airdrops/rarityfarming/szn10/rnd4.ts
@@ -0,0 +1,27 @@
+import { run } from "hardhat";
+import { RarityPayoutTaskArgs } from "../../../../tasks/rarityPayouts";
+
+async function rarityPayout() {
+  const args: RarityPayoutTaskArgs = {
+    season: "10",
+    rarityDataFile: "rnd4",
+    rounds: "4",
+    totalAmount: "1200000",
+    blockNumber: "",
+    deployerAddress: "0x821049b2273b0ccd34a64d1b08a3346f110ecae2",
+    tieBreakerIndex: "3",
+    rarityParams: [750000.0, 7500, 0.94].toString(),
+    kinshipParams: [300000.0, 7500, 0.76].toString(),
+    xpParams: [150000.0, 7500, 0.65].toString(),
+  };
+  await run("rarityPayout", args);
+}
+
+rarityPayout()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+exports.rarityPayout = rarityPayout;
